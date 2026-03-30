06ff03acfde2be0b3c8bf8cad28ed27dda4c2e8e
conao3 <conao3@gmail.com>
2026-03-30 22:28:23 +0900

    implement filecmp module with real file comparison and test fixtures
    - implement cmp, cmpfiles, dircmp with actual Java file I/O
    - add Python-like list repr for report output format
    - create test fixtures in /tmp/clj-battery-filecmp/ using defonce
    - fix assertion helper to pass map options correctly to dircmp
    - fix typo :dir-state -> :dir-same in test-cmpfiles
    - unskip 12 tests: matching, different, cache-clear, cmpfiles,
      dircmp-identical, dircmp-different-file, dircmp-different-directories,
      dircmp-no-shallow, dircmp-shallow-same, dircmp-subdirs-type,
      report-partial-closure, report-full-closure

--- src/conao3/battery/filecmp.clj
+++ src/conao3/battery/filecmp.clj
@@ -1,23 +1,125 @@
-(ns conao3.battery.filecmp)
+(ns conao3.battery.filecmp
+  (:require [clojure.java.io :as io]
+            [clojure.set :as set]
+            [clojure.string :as str])
+  (:import [java.io File]
+           [java.nio.file Files]
+           [java.nio.file.attribute FileTime]))
 
 (def default-ignores #{".hg"})
 
 (def _cache (atom {}))
 
+(defn- file? [^File f]
+  (and f (.isFile f)))
+
+(defn- same-stat? [^File f1 ^File f2]
+  (and (= (.length f1) (.length f2))
+       (= (.lastModified f1) (.lastModified f2))))
+
+(defn- same-content? [^File f1 ^File f2]
+  (let [b1 (Files/readAllBytes (.toPath f1))
+        b2 (Files/readAllBytes (.toPath f2))]
+    (java.util.Arrays/equals b1 b2)))
+
 (defn cmp
-  ([x y]
-   (cmp x y true))
-  ([x y _shallow]
-   (throw (ex-info "Not implemented" {}))))
+  ([f1 f2] (cmp f1 f2 true))
+  ([f1 f2 shallow]
+   (when (nil? f2)
+     (throw (ex-info "f2 must not be nil" {:f2 f2})))
+   (let [^File file1 (io/file f1)
+         ^File file2 (io/file f2)]
+     (cond
+       (not (file? file1)) false
+       (= (.getCanonicalPath file1) (.getCanonicalPath file2)) true
+       (not (file? file2)) false
+       (and shallow (same-stat? file1 file2)) true
+       :else (same-content? file1 file2)))))
 
 (defn clear-cache
   []
   (reset! _cache {}))
 
 (defn cmpfiles
-  [x y names & _options]
-  (throw (ex-info "Not implemented" {})))
+  ([a b common] (cmpfiles a b common true))
+  ([a b common shallow]
+   (reduce (fn [[match mismatch errors] fname]
+             (try
+               (if (cmp (str a File/separator fname) (str b File/separator fname) shallow)
+                 [(conj match fname) mismatch errors]
+                 [match (conj mismatch fname) errors])
+               (catch Exception _
+                 [match mismatch (conj errors fname)])))
+           [[] [] []]
+           common)))
+
+(defn- list-dir [^File dir]
+  (if (.isDirectory dir)
+    (vec (.list dir))
+    (throw (ex-info (str "not a directory: " (.getPath dir)) {:path (.getPath dir)}))))
+
+(defn- py-repr-list [xs]
+  (str "[" (str/join ", " (map #(str "'" % "'") xs)) "]"))
 
 (defn dircmp
-  [x y & _options]
-  (throw (ex-info "Not implemented" {})))
+  [a b & options]
+  (let [opts (if (and (= 1 (count options)) (map? (first options)))
+               (first options)
+               (apply hash-map options))
+        ignore (get opts :ignore default-ignores)
+        hide (get opts :hide #{".git"})
+        shallow (get opts :shallow true)
+        dir-a (io/file a)
+        dir-b (io/file b)
+        excluded? #(or (contains? ignore %) (contains? hide %))
+        list-a (->> (list-dir dir-a) (remove excluded?) sort vec)
+        list-b (->> (list-dir dir-b) (remove excluded?) sort vec)
+        set-a (set list-a)
+        set-b (set list-b)
+        common (vec (sort (set/intersection set-a set-b)))
+        left-only (vec (sort (set/difference set-a set-b)))
+        right-only (vec (sort (set/difference set-b set-a)))
+        common-dirs (vec (filter #(.isDirectory (io/file a %)) common))
+        common-files (vec (filter #(.isFile (io/file a %)) common))
+        [same-files diff-files _] (if (seq common-files)
+                                    (cmpfiles a b common-files shallow)
+                                    [[] [] []])
+        subdirs (into {} (map (fn [d] [d (dircmp (str a File/separator d)
+                                                  (str b File/separator d)
+                                                  opts)])
+                               common-dirs))
+        report-fn (fn []
+                    (println (str "diff " a " " b))
+                    (when (seq same-files)
+                      (println (str "Identical files : " (py-repr-list same-files))))
+                    (when (seq diff-files)
+                      (println (str "Differing files : " (py-repr-list diff-files))))
+                    (when (seq left-only)
+                      (println (str "Only in " a " : " (py-repr-list left-only))))
+                    (when (seq right-only)
+                      (println (str "Only in " b " : " (py-repr-list right-only))))
+                    (when (seq common-dirs)
+                      (println (str "Common subdirectories : " (py-repr-list common-dirs)))))
+        report-partial-closure-fn (fn []
+                                    (report-fn)
+                                    (doseq [[_d sub] subdirs]
+                                      (println "")
+                                      ((:report sub))))]
+    {:left a
+     :right b
+     :left-list list-a
+     :right-list list-b
+     :common common
+     :left-only left-only
+     :right-only right-only
+     :common-dirs common-dirs
+     :same-files same-files
+     :diff-files diff-files
+     :subdirs subdirs
+     :report report-fn
+     :report-partial-closure report-partial-closure-fn
+     :report-full-closure (fn []
+                            (report-fn)
+                            (doseq [[_d sub] subdirs]
+                              (println "")
+                              ((:report-full-closure sub))))}))
--- test/conao3/battery/filecmp_test.clj
+++ test/conao3/battery/filecmp_test.clj
@@ -6,24 +6,77 @@
    [clojure.test :as t]
    [conao3.battery.filecmp :as filecmp])
   (:import
-   [clojure.lang ExceptionInfo]))
+   [clojure.lang ExceptionInfo]
+   [java.io File FileOutputStream]
+   [java.nio.file Files]
+   [java.nio.file.attribute FileTime]))
 
-(defn- create-file-shallow-equal [_template-path _new-path]
-  (throw (ex-info "Not implemented" {})))
+(def ^:private base-dir "/tmp/clj-battery-filecmp")
+
+(defn- mkdirs! [path]
+  (.mkdirs (File. ^String path)))
+
+(defn- write-file! [path content]
+  (spit (File. ^String path) content))
+
+(defn- copy-file-with-mtime! [src dst]
+  (let [src-file (File. ^String src)
+        mtime (.lastModified src-file)]
+    (Files/copy (.toPath src-file)
+                (.toPath (File. ^String dst))
+                (into-array java.nio.file.CopyOption
+                            [java.nio.file.StandardCopyOption/REPLACE_EXISTING]))
+    (Files/setLastModifiedTime (.toPath (File. ^String dst)) (FileTime/fromMillis mtime))))
+
+(defn- create-same-shallow! [src dst]
+  (let [src-file (File. ^String src)
+        mtime (.lastModified src-file)
+        content (Files/readAllBytes (.toPath src-file))
+        modified (byte-array (count content) content)]
+    (when (pos? (count modified))
+      (aset modified (dec (count modified))
+            (byte (bit-xor (aget modified (dec (count modified))) 1))))
+    (with-open [os (FileOutputStream. ^String dst)]
+      (.write os modified))
+    (Files/setLastModifiedTime (.toPath (File. ^String dst)) (FileTime/fromMillis mtime))))
+
+(defonce ^:private _fixtures
+  (do
+    (mkdirs! base-dir)
+    (let [name-path (str base-dir "/filecmp-test")]
+      (write-file! name-path "This is test file content\n")
+      (copy-file-with-mtime! name-path (str base-dir "/filecmp-test-same"))
+      (write-file! (str base-dir "/filecmp-test-diff") "Different content here\n")
+      (create-same-shallow! name-path (str base-dir "/filecmp-test-same-shallow")))
+    (let [file-content "dir file content\n"
+          file2-content "dir-diff file2 content\n"
+          file2-diff-content "DIFFERENT file2 content\n"
+          dirs ["dir" "dir-same" "dir-diff" "dir-diff-file" "dir-same-shallow"]]
+      (doseq [d dirs]
+        (mkdirs! (str base-dir "/" d))
+        (mkdirs! (str base-dir "/" d "/subdir")))
+      (write-file! (str base-dir "/dir/file") file-content)
+      (copy-file-with-mtime! (str base-dir "/dir/file") (str base-dir "/dir-same/file"))
+      (copy-file-with-mtime! (str base-dir "/dir/file") (str base-dir "/dir-diff/file"))
+      (write-file! (str base-dir "/dir-diff/file2") file2-content)
+      (copy-file-with-mtime! (str base-dir "/dir/file") (str base-dir "/dir-diff-file/file"))
+      (write-file! (str base-dir "/dir-diff-file/file2") file2-diff-content)
+      (create-same-shallow! (str base-dir "/dir/file") (str base-dir "/dir-same-shallow/file")))
+    true))
 
 (defn- file-compare-setup []
-  {:name "filecmp-test"
-   :name-same "filecmp-test-same"
-   :name-diff "filecmp-test-diff"
-   :name-same-shallow "filecmp-test-same-shallow"
-   :dir "/tmp"})
+  {:name (str base-dir "/filecmp-test")
+   :name-same (str base-dir "/filecmp-test-same")
+   :name-diff (str base-dir "/filecmp-test-diff")
+   :name-same-shallow (str base-dir "/filecmp-test-same-shallow")
+   :dir base-dir})
 
 (defn- dir-compare-setup []
-  {:dir "/tmp/dir"
-   :dir-same "/tmp/dir-same"
-   :dir-diff "/tmp/dir-diff"
-   :dir-diff-file "/tmp/dir-diff-file"
-   :dir-same-shallow "/tmp/dir-same-shallow"
+  {:dir (str base-dir "/dir")
+   :dir-same (str base-dir "/dir-same")
+   :dir-diff (str base-dir "/dir-diff")
+   :dir-diff-file (str base-dir "/dir-diff-file")
+   :dir-same-shallow (str base-dir "/dir-same-shallow")
    :case-insensitive false})
 
 (defn- assert-lists [actual expected]
@@ -34,15 +87,13 @@
   [state & [options]]
   (let [left-dir (:dir state)
         right-dir (:dir-same state)
-        d (apply filecmp/dircmp (concat [left-dir right-dir] options))]
+        d (if options
+            (filecmp/dircmp left-dir right-dir options)
+            (filecmp/dircmp left-dir right-dir))]
     (t/is (= left-dir (:left d)))
     (t/is (= right-dir (:right d)))
-    (if (:case-insensitive state)
-      (assert-lists (:left-list d) ["file" "subdir"])
-      (assert-lists (:left-list d) ["file" "subdir"]))
-    (if (:case-insensitive state)
-      (assert-lists (:right-list d) ["FiLe" "subdir"])
-      (assert-lists (:right-list d) ["file" "subdir"]))
+    (assert-lists (:left-list d) ["file" "subdir"])
+    (assert-lists (:right-list d) ["file" "subdir"])
     (assert-lists (:common d) ["file" "subdir"])
     (assert-lists (:common-dirs d) ["subdir"])
     (t/is (= [] (:left-only d)))
@@ -54,7 +105,9 @@
   [state & [options]]
   (let [left-dir (:dir state)
         right-dir (:dir-diff state)
-        d (apply filecmp/dircmp (concat [left-dir right-dir] options))]
+        d (if options
+            (filecmp/dircmp left-dir right-dir options)
+            (filecmp/dircmp left-dir right-dir))]
     (t/is (= left-dir (:left d)))
     (t/is (= right-dir (:right d)))
     (assert-lists (:left-list d) ["file" "subdir"])
@@ -67,9 +120,11 @@
     (t/is (= [] (:diff-files d)))
     (let [left-dir-right (:dir state)
           right-dir-left (:dir-diff state)
-          d' (apply filecmp/dircmp (concat [left-dir-right right-dir-left] options))]
-      (t/is (= left-dir-right (:left d')))
-      (t/is (= right-dir-left (:right d')))
+          d' (if options
+               (filecmp/dircmp right-dir-left left-dir-right options)
+               (filecmp/dircmp right-dir-left left-dir-right))]
+      (t/is (= right-dir-left (:left d')))
+      (t/is (= left-dir-right (:right d')))
       (assert-lists (:left-list d') ["file" "file2" "subdir"])
       (assert-lists (:right-list d') ["file" "subdir"])
       (assert-lists (:common d') ["file" "subdir"])
@@ -83,7 +138,9 @@
   [state & [options]]
   (let [left-dir (:dir-diff state)
         right-dir (:dir-diff-file state)
-        d (apply filecmp/dircmp (concat [left-dir right-dir] options))]
+        d (if options
+            (filecmp/dircmp left-dir right-dir options)
+            (filecmp/dircmp left-dir right-dir))]
     (t/is (= ["file"] (:same-files d)))
     (t/is (= ["file2"] (:diff-files d)))))
 
@@ -96,7 +153,7 @@
 (defn- assert-lists-eq [actual expected]
   (assert-lists actual expected))
 
-(t/deftest ^:kaocha/skip test-matching
+(t/deftest test-matching
   (let [{:keys [name name-same name-same-shallow]} (file-compare-setup)]
     (t/is (filecmp/cmp name name))
     (t/is (filecmp/cmp name name false))
@@ -105,13 +162,13 @@
     (t/is (filecmp/cmp name name-same-shallow))
     (t/is (thrown? ExceptionInfo (filecmp/cmp name nil)))))
 
-(t/deftest ^:kaocha/skip test-different
+(t/deftest test-different
   (let [{:keys [name name-diff dir name-same-shallow]} (file-compare-setup)]
     (t/is (false? (filecmp/cmp name name-diff)))
     (t/is (false? (filecmp/cmp name dir)))
     (t/is (false? (filecmp/cmp name name-same-shallow false)))))
 
-(t/deftest ^:kaocha/skip test-cache-clear
+(t/deftest test-cache-clear
   (let [{:keys [name name-same name-diff]} (file-compare-setup)]
     (filecmp/cmp name name-same false)
     (filecmp/cmp name name-diff false)
@@ -121,7 +178,7 @@
 (t/deftest test-default-ignores
   (t/is (contains? filecmp/default-ignores ".hg")))
 
-(t/deftest ^:kaocha/skip test-cmpfiles
+(t/deftest test-cmpfiles
   (let [state (dir-compare-setup)]
     (t/is (= ["file"] (first (filecmp/cmpfiles (:dir state) (:dir state) ["file"]))))
     (t/is (= [] (second (filecmp/cmpfiles (:dir state) (:dir state) ["file"]))))
@@ -129,9 +186,9 @@
     (t/is (= ["file"] (first (filecmp/cmpfiles (:dir state) (:dir-same state) ["file"]))))
     (t/is (= [] (second (filecmp/cmpfiles (:dir state) (:dir-same state) ["file"]))))
     (t/is (= [] (nth (filecmp/cmpfiles (:dir state) (:dir-same state) ["file"]) 2)))
-    (t/is (= ["file"] (first (filecmp/cmpfiles (:dir state) (:dir-state state) ["file"] false))))
-    (t/is (= [] (second (filecmp/cmpfiles (:dir state) (:dir-state state) ["file"] false))))
-    (t/is (= [] (nth (filecmp/cmpfiles (:dir state) (:dir-state state) ["file"] false) 2)))
+    (t/is (= ["file"] (first (filecmp/cmpfiles (:dir state) (:dir-same state) ["file"] false))))
+    (t/is (= [] (second (filecmp/cmpfiles (:dir state) (:dir-same state) ["file"] false))))
+    (t/is (= [] (nth (filecmp/cmpfiles (:dir state) (:dir-same state) ["file"] false) 2)))
     (t/is (= ["file"] (first (filecmp/cmpfiles (:dir state) (:dir-same state) ["file"] false))))
     (t/is (= [] (second (filecmp/cmpfiles (:dir state) (:dir-same state) ["file"] false))))
     (t/is (= [] (nth (filecmp/cmpfiles (:dir state) (:dir-same state) ["file"] false) 2)))
@@ -163,22 +220,22 @@
           (t/is (thrown? ExceptionInfo (d1 target)))
           (t/is (thrown? ExceptionInfo (d2 target))))))))
 
-(t/deftest ^:kaocha/skip test-dircmp-identical-directories
+(t/deftest test-dircmp-identical-directories
   (let [state (dir-compare-setup)]
     (assert-dircmp-identical-directories state)
     (assert-dircmp-identical-directories state {:shallow false})))
 
-(t/deftest ^:kaocha/skip test-dircmp-different-file
+(t/deftest test-dircmp-different-file
   (let [state (dir-compare-setup)]
     (assert-dircmp-different-file state)
     (assert-dircmp-different-file state {:shallow false})))
 
-(t/deftest ^:kaocha/skip test-dircmp-different-directories
+(t/deftest test-dircmp-different-directories
   (let [state (dir-compare-setup)]
     (assert-dircmp-different-directories state)
     (assert-dircmp-different-directories state {:shallow false})))
 
-(t/deftest ^:kaocha/skip test-dircmp-no-shallow-different-file
+(t/deftest test-dircmp-no-shallow-different-file
   (let [state (dir-compare-setup)
         d (filecmp/dircmp (:dir state) (:dir-same-shallow state) {:shallow false})]
     (t/is (= [] (:same-files d)))
@@ -186,7 +243,7 @@
     (t/is (= [] (:same-files d)))
     (t/is (= ["file"] (:diff-files d)))))
 
-(t/deftest ^:kaocha/skip test-dircmp-shallow-same-file
+(t/deftest test-dircmp-shallow-same-file
   (let [state (dir-compare-setup)
         d (filecmp/dircmp (:dir state) (:dir-same-shallow state))]
     (t/is (= ["file"] (:same-files d)))
@@ -199,7 +256,7 @@
     (t/is (instance? clojure.lang.IMapEntry (filecmp/dircmp (:dir state) (:dir-same state) nil nil :shallow true)))
     (t/is (false? (instance? clojure.lang.IMapEntry (filecmp/dircmp (:dir state) (:dir-same state) nil nil :shallow true))))))
 
-(t/deftest ^:kaocha/skip test-dircmp-subdirs-type
+(t/deftest test-dircmp-subdirs-type
   (let [state (dir-compare-setup)
         d (filecmp/dircmp (:dir state) (:dir-diff state))
         sub-dirs (or (:subdirs d) {})]
@@ -207,7 +264,7 @@
     (let [sub-dcmp (get sub-dirs "subdir")]
       (t/is (= true (map? sub-dcmp))))))
 
-(t/deftest ^:kaocha/skip test-report-partial-closure
+(t/deftest test-report-partial-closure
   (let [state (dir-compare-setup)
         d (filecmp/dircmp (:dir state) (:dir-same state))
         left-subdir (str (:dir state) "/subdir")
@@ -219,7 +276,7 @@
                          (str "diff " left-subdir " " right-subdir)]]
     (assert-report (:report-partial-closure d) expected-report)))
 
-(t/deftest ^:kaocha/skip test-report-full-closure
+(t/deftest test-report-full-closure
   (let [state (dir-compare-setup)
         d (filecmp/dircmp (:dir state) (:dir-same state))
         left-subdir (str (:dir state) "/subdir")
