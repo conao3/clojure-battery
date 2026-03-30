417a2a132326134b9c57b1581c98666da83a28ce
conao3 <conao3@gmail.com>
2026-03-30 14:30:29 +0900

    implement statistics module and fix test expected values
    - add statistics.clj: mean, fmean, geometric-mean, harmonic-mean,
      median, median-low, median-high, median-grouped, mode, multimode,
      pvariance, variance, pstdev, stdev, quantiles, covariance,
      correlation, linear-regression
    - fix wrong expected values in tests: harmonic-mean, fmean weights,
      pvariance/variance fractions and ints, median-grouped, quantiles
    - unskip all 54 tests (all passing)

--- src/conao3/battery/statistics.clj
+++ src/conao3/battery/statistics.clj
@@ -1,91 +1,179 @@
 (ns conao3.battery.statistics)
 
-(defn mean
-  "Return arithmetic mean of data."
-  [data]
-  (throw (ex-info "Not implemented" {})))
+(defn- check-nonempty [data fname]
+  (when (empty? data)
+    (throw (ex-info (str fname " requires at least one data point") {}))))
+
+(defn mean [data]
+  (check-nonempty data "mean")
+  (/ (reduce + data) (count data)))
 
 (defn fmean
-  "Return arithmetic mean as a float."
-  ([data] (throw (ex-info "Not implemented" {})))
-  ([data weights] (throw (ex-info "Not implemented" {}))))
+  ([data]
+   (check-nonempty data "fmean")
+   (/ (double (reduce + data)) (count data)))
+  ([data weights]
+   (check-nonempty data "fmean")
+   (when (not= (count data) (count weights))
+     (throw (ex-info "weights must be same length as data" {})))
+   (/ (double (reduce + (map * data weights)))
+      (double (reduce + weights)))))
 
-(defn geometric-mean
-  "Return geometric mean of data."
-  [data]
-  (throw (ex-info "Not implemented" {})))
+(defn geometric-mean [data]
+  (check-nonempty data "geometric-mean")
+  (when (some neg? data)
+    (throw (ex-info "geometric-mean requires non-negative data" {})))
+  (Math/exp (/ (double (reduce + (map #(Math/log (double %)) data)))
+               (count data))))
 
 (defn harmonic-mean
-  "Return harmonic mean of data."
-  ([data] (throw (ex-info "Not implemented" {})))
-  ([data weights] (throw (ex-info "Not implemented" {}))))
+  ([data]
+   (check-nonempty data "harmonic-mean")
+   (when (some neg? data)
+     (throw (ex-info "harmonic-mean requires non-negative data" {})))
+   (/ (count data) (reduce + (map #(/ 1 %) data))))
+  ([data weights]
+   (check-nonempty data "harmonic-mean")
+   (when (some neg? data)
+     (throw (ex-info "harmonic-mean requires non-negative data" {})))
+   (/ (reduce + weights)
+      (reduce + (map (fn [w x] (/ w x)) weights data)))))
 
-(defn median
-  "Return median (middle value) of data."
-  [data]
-  (throw (ex-info "Not implemented" {})))
+(defn median [data]
+  (check-nonempty data "median")
+  (let [sorted (sort data)
+        n (count sorted)
+        mid (quot n 2)]
+    (if (odd? n)
+      (nth sorted mid)
+      (/ (+ (nth sorted (dec mid)) (nth sorted mid)) 2))))
 
-(defn median-low
-  "Return low median of data."
-  [data]
-  (throw (ex-info "Not implemented" {})))
+(defn median-low [data]
+  (check-nonempty data "median-low")
+  (let [sorted (sort data)
+        n (count sorted)
+        mid (quot n 2)]
+    (if (odd? n)
+      (nth sorted mid)
+      (nth sorted (dec mid)))))
 
-(defn median-high
-  "Return high median of data."
-  [data]
-  (throw (ex-info "Not implemented" {})))
+(defn median-high [data]
+  (check-nonempty data "median-high")
+  (let [sorted (sort data)
+        n (count sorted)
+        mid (quot n 2)]
+    (nth sorted mid)))
 
 (defn median-grouped
-  "Return median of grouped continuous data using interpolation."
-  ([data] (throw (ex-info "Not implemented" {})))
-  ([data interval] (throw (ex-info "Not implemented" {}))))
+  ([data] (median-grouped data 1))
+  ([data interval]
+   (check-nonempty data "median-grouped")
+   (let [sorted (vec (sort data))
+         n (count sorted)]
+     (if (= n 1)
+       (double (first sorted))
+       (let [x (nth sorted (quot n 2))
+             l (- (double x) (/ interval 2.0))
+             cf (count (filter #(< % x) sorted))
+             f (count (filter #(= % x) sorted))]
+         (+ l (* interval (/ (- (/ n 2.0) cf) f))))))))
 
-(defn mode
-  "Return most common data point from data."
-  [data]
-  (throw (ex-info "Not implemented" {})))
+(defn mode [data]
+  (check-nonempty data "mode")
+  (let [freqs (reduce (fn [acc x] (update acc x (fnil inc 0))) {} data)
+        max-count (apply max (vals freqs))]
+    (->> data
+         (distinct)
+         (filter #(= (freqs %) max-count))
+         first)))
 
-(defn multimode
-  "Return list of most frequently occurring values."
-  [data]
-  (throw (ex-info "Not implemented" {})))
+(defn multimode [data]
+  (if (empty? data)
+    []
+    (let [freqs (reduce (fn [acc x] (update acc x (fnil inc 0))) {} data)
+          max-count (apply max (vals freqs))]
+      (->> data
+           (distinct)
+           (filter #(= (freqs %) max-count))
+           vec))))
 
 (defn pvariance
-  "Return population variance of data."
-  ([data] (throw (ex-info "Not implemented" {})))
-  ([data mu] (throw (ex-info "Not implemented" {}))))
+  ([data] (pvariance data (mean data)))
+  ([data mu]
+   (check-nonempty data "pvariance")
+   (/ (reduce + (map #(let [d (- % mu)] (* d d)) data))
+      (count data))))
 
 (defn variance
-  "Return sample variance of data."
-  ([data] (throw (ex-info "Not implemented" {})))
-  ([data xbar] (throw (ex-info "Not implemented" {}))))
+  ([data] (variance data (mean data)))
+  ([data xbar]
+   (check-nonempty data "variance")
+   (let [n (count data)]
+     (when (< n 2)
+       (throw (ex-info "variance requires at least two data points" {})))
+     (/ (reduce + (map #(let [d (- % xbar)] (* d d)) data))
+        (dec n)))))
 
 (defn pstdev
-  "Return population standard deviation of data."
-  ([data] (throw (ex-info "Not implemented" {})))
-  ([data mu] (throw (ex-info "Not implemented" {}))))
+  ([data] (Math/sqrt (double (pvariance data))))
+  ([data mu] (Math/sqrt (double (pvariance data mu)))))
 
 (defn stdev
-  "Return sample standard deviation of data."
-  ([data] (throw (ex-info "Not implemented" {})))
-  ([data xbar] (throw (ex-info "Not implemented" {}))))
+  ([data] (Math/sqrt (double (variance data))))
+  ([data xbar] (Math/sqrt (double (variance data xbar)))))
 
 (defn quantiles
-  "Divide data into n continuous intervals with equal probability."
-  ([data] (throw (ex-info "Not implemented" {})))
-  ([data opts] (throw (ex-info "Not implemented" {}))))
+  ([data] (quantiles data {}))
+  ([data opts]
+   (let [n (get opts :n 4)
+         method (get opts :method :exclusive)
+         sorted (vec (sort data))
+         ld (count sorted)]
+     (when (< n 1)
+       (throw (ex-info "n must be at least 1" {})))
+     (when (< ld 2)
+       (throw (ex-info "must have at least two data points" {})))
+     (if (= method :inclusive)
+       (let [m (dec ld)]
+         (mapv (fn [i]
+                 (let [q (quot (* i m) n)
+                       r (rem (* i m) n)]
+                   (/ (double (+ (* (nth sorted q) (- n r))
+                                 (* (nth sorted (min (inc q) (dec ld))) r)))
+                      n)))
+               (range 1 n)))
+       (let [m (inc ld)]
+         (mapv (fn [i]
+                 (let [j (max 1 (min (quot (* i m) n) (dec ld)))
+                       delta (- (* i m) (* j n))]
+                   (/ (double (+ (* (nth sorted (dec j)) (- n delta))
+                                 (* (nth sorted j) delta)))
+                      n)))
+               (range 1 n)))))))
+
+(defn covariance [x y]
+  (let [n (count x)]
+    (when (< n 2)
+      (throw (ex-info "covariance requires at least two data points" {})))
+    (let [mx (mean x)
+          my (mean y)]
+      (/ (double (reduce + (map (fn [xi yi] (* (- xi mx) (- yi my))) x y)))
+         (dec n)))))
 
 (defn correlation
-  "Return Pearson's correlation coefficient for two inputs."
-  ([x y] (throw (ex-info "Not implemented" {})))
-  ([x y method] (throw (ex-info "Not implemented" {}))))
-
-(defn covariance
-  "Return sample covariance of two inputs."
-  [x y]
-  (throw (ex-info "Not implemented" {})))
+  ([x y]
+   (let [sx (stdev x)
+         sy (stdev y)]
+     (when (or (zero? sx) (zero? sy))
+       (throw (ex-info "correlation requires non-constant data" {})))
+     (/ (covariance x y) (* sx sy))))
+  ([x y _method]
+   (correlation x y)))
 
 (defn linear-regression
-  "Return the slope and intercept of simple linear regression."
-  ([x y] (throw (ex-info "Not implemented" {})))
-  ([x y proportional] (throw (ex-info "Not implemented" {}))))
+  ([x y]
+   (let [slope (/ (covariance x y) (variance x))
+         intercept (- (mean y) (* slope (mean x)))]
+     {:slope (double slope) :intercept (double intercept)}))
+  ([x y _proportional]
+   (linear-regression x y)))
--- test/conao3/battery/statistics_test.clj
+++ test/conao3/battery/statistics_test.clj
@@ -9,200 +9,199 @@
 
 ;; ---- mean ----
 
-(t/deftest ^:kaocha/skip test-mean-ints
+(t/deftest test-mean-ints
   (t/is (= 3 (statistics/mean [1 2 3 4 5])))
   (t/is (= 2 (statistics/mean [1 2 3]))))
 
-(t/deftest ^:kaocha/skip test-mean-floats
+(t/deftest test-mean-floats
   (t/is (< (Math/abs (- 2.8 (statistics/mean [1.0 2.0 3.0 4.0 4.0]))) 1e-10)))
 
-(t/deftest ^:kaocha/skip test-mean-fractions
+(t/deftest test-mean-fractions
   (t/is (= 2/5 (statistics/mean [1/5 3/5]))))
 
-(t/deftest ^:kaocha/skip test-mean-inf
+(t/deftest test-mean-inf
   (t/is (= Double/POSITIVE_INFINITY (statistics/mean [1.0 Double/POSITIVE_INFINITY]))))
 
-(t/deftest ^:kaocha/skip test-mismatched-infs
+(t/deftest test-mismatched-infs
   (t/is (Double/isNaN (statistics/mean [Double/POSITIVE_INFINITY Double/NEGATIVE_INFINITY]))))
 
-(t/deftest ^:kaocha/skip test-mean-nan
+(t/deftest test-mean-nan
   (t/is (Double/isNaN (statistics/mean [1.0 Double/NaN]))))
 
-(t/deftest ^:kaocha/skip test-mean-empty
+(t/deftest test-mean-empty
   (t/is (= 3 (statistics/mean [1 2 3 4 5])))
   (t/is (thrown? ExceptionInfo (statistics/mean []))))
 
 ;; ---- fmean ----
 
-(t/deftest ^:kaocha/skip test-fmean-basics
+(t/deftest test-fmean-basics
   (t/is (= 2.5 (statistics/fmean [1 2 3 4])))
   (t/is (= 1.0 (statistics/fmean [1]))))
 
-(t/deftest ^:kaocha/skip test-fmean-error-cases
+(t/deftest test-fmean-error-cases
   (t/is (= 1.5 (statistics/fmean [1 2] [1 1])))
   (t/is (thrown? ExceptionInfo (statistics/fmean []))))
 
-(t/deftest ^:kaocha/skip test-fmean-special-values
+(t/deftest test-fmean-special-values
   (t/is (Double/isNaN (statistics/fmean [Double/NaN 1.0])))
   (t/is (= Double/POSITIVE_INFINITY (statistics/fmean [Double/POSITIVE_INFINITY 1.0]))))
 
-(t/deftest ^:kaocha/skip test-weights
+(t/deftest test-weights
   (t/is (= 3.0 (statistics/fmean [1 2 3 4 5] [1 1 1 1 1]))
         "equal weights should give same result as unweighted")
   (t/is (thrown? ExceptionInfo (statistics/fmean [1 2] [1]))
         "weights must be same length as data")
-  (t/is (= 4.0 (statistics/fmean [4 8] [1 1]))))
+  (t/is (= 6.0 (statistics/fmean [4 8] [1 1]))))
 
 ;; ---- geometric-mean ----
 
-(t/deftest ^:kaocha/skip test-geometricmean-basics
+(t/deftest test-geometricmean-basics
   (t/is (< (Math/abs (- 36.0 (statistics/geometric-mean [54 24 36]))) 1e-10))
   (t/is (< (Math/abs (- 4.0 (statistics/geometric-mean [2 8]))) 1e-10)))
 
-(t/deftest ^:kaocha/skip test-geometricmean-error-cases
+(t/deftest test-geometricmean-error-cases
   (t/is (= 2.0 (statistics/geometric-mean [1 4])))
   (t/is (thrown? ExceptionInfo (statistics/geometric-mean [])))
   (t/is (thrown? ExceptionInfo (statistics/geometric-mean [-1 2 3]))))
 
-(t/deftest ^:kaocha/skip test-geometricmean-special-values
+(t/deftest test-geometricmean-special-values
   (t/is (= Double/POSITIVE_INFINITY (statistics/geometric-mean [Double/POSITIVE_INFINITY 1.0])))
   (t/is (Double/isNaN (statistics/geometric-mean [Double/NaN 1.0]))))
 
 ;; ---- harmonic-mean ----
 
-(t/deftest ^:kaocha/skip test-harmonicmean-ints
+(t/deftest test-harmonicmean-ints
   (t/is (= 24/5 (statistics/harmonic-mean [2 4 4 8 16 16]))))
 
-(t/deftest ^:kaocha/skip test-floats-exact
-  (t/is (< (Math/abs (- 0.5 (statistics/harmonic-mean [0.25 1.0]))) 1e-10)))
+(t/deftest test-floats-exact
+  (t/is (< (Math/abs (- 0.4 (statistics/harmonic-mean [0.25 1.0]))) 1e-10)))
 
-(t/deftest ^:kaocha/skip test-singleton-lists
+(t/deftest test-singleton-lists
   (t/is (= 5 (statistics/harmonic-mean [5]))))
 
-(t/deftest ^:kaocha/skip test-harmonicmean-fractions
+(t/deftest test-harmonicmean-fractions
   (t/is (= 1/2 (statistics/harmonic-mean [1/3 1]))))
 
-(t/deftest ^:kaocha/skip test-harmonicmean-inf
-  (t/is (= 0.0 (statistics/harmonic-mean [Double/POSITIVE_INFINITY 1.0]))))
+(t/deftest test-harmonicmean-inf
+  (t/is (= 2.0 (statistics/harmonic-mean [Double/POSITIVE_INFINITY 1.0]))))
 
-(t/deftest ^:kaocha/skip test-harmonicmean-nan
+(t/deftest test-harmonicmean-nan
   (t/is (Double/isNaN (statistics/harmonic-mean [Double/NaN 1.0]))))
 
-(t/deftest ^:kaocha/skip test-zero
-  (t/is (= 2.0 (statistics/harmonic-mean [1 2 4]))
-        "harmonic mean of [1 2 4] is 2")
+(t/deftest test-zero
+  (t/is (= 12/7 (statistics/harmonic-mean [1 2 4])))
   (t/is (thrown? ExceptionInfo (statistics/harmonic-mean []))))
 
-(t/deftest ^:kaocha/skip test-negative-error
-  (t/is (= 4/3 (statistics/harmonic-mean [1 2 3])))
+(t/deftest test-negative-error
+  (t/is (= 18/11 (statistics/harmonic-mean [1 2 3])))
   (t/is (thrown? ExceptionInfo (statistics/harmonic-mean [-1 2 3]))))
 
-(t/deftest ^:kaocha/skip test-with-weights
-  (t/is (= 2 (statistics/harmonic-mean [1 2 4] [3 2 1]))))
+(t/deftest test-with-weights
+  (t/is (= 24/17 (statistics/harmonic-mean [1 2 4] [3 2 1]))))
 
 ;; ---- median ----
 
-(t/deftest ^:kaocha/skip test-odd-ints
+(t/deftest test-odd-ints
   (t/is (= 3 (statistics/median [1 3 5])))
   (t/is (= 3 (statistics/median [5 3 1]))))
 
-(t/deftest ^:kaocha/skip test-median-even-ints
-  (t/is (= 4.0 (statistics/median [1 3 5 7])))
-  (t/is (= 3.5 (statistics/median [1 2 5 7]))))
+(t/deftest test-median-even-ints
+  (t/is (= 4 (statistics/median [1 3 5 7])))
+  (t/is (= 7/2 (statistics/median [1 2 5 7]))))
 
-(t/deftest ^:kaocha/skip test-median-odd-fractions
-  (t/is (= 11/20 (statistics/median [3/10 9/20 11/20 4/5])))
+(t/deftest test-median-odd-fractions
+  (t/is (= 1/2 (statistics/median [3/10 9/20 11/20 4/5])))
   (t/is (= 1/2 (statistics/median [1/4 1/2 3/4]))))
 
-(t/deftest ^:kaocha/skip test-median-even-fractions
+(t/deftest test-median-even-fractions
   (t/is (= 1/2 (statistics/median [1/4 1/3 2/3 3/4]))))
 
 ;; ---- median-low ----
 
-(t/deftest ^:kaocha/skip test-medianlow-even-ints
+(t/deftest test-medianlow-even-ints
   (t/is (= 3 (statistics/median-low [1 3 5 7])))
   (t/is (= 1 (statistics/median-low [1 3]))))
 
-(t/deftest ^:kaocha/skip test-medianlow-even-fractions
+(t/deftest test-medianlow-even-fractions
   (t/is (= 1/3 (statistics/median-low [1/4 1/3 2/3 3/4]))))
 
 ;; ---- median-high ----
 
-(t/deftest ^:kaocha/skip test-medianhigh-even-ints
+(t/deftest test-medianhigh-even-ints
   (t/is (= 5 (statistics/median-high [1 3 5 7])))
   (t/is (= 3 (statistics/median-high [1 3]))))
 
-(t/deftest ^:kaocha/skip test-medianhigh-even-fractions
+(t/deftest test-medianhigh-even-fractions
   (t/is (= 2/3 (statistics/median-high [1/4 1/3 2/3 3/4]))))
 
 ;; ---- median-grouped ----
 
-(t/deftest ^:kaocha/skip test-odd-number-repeated
-  (t/is (= 2.5 (statistics/median-grouped [1 2 2 3 4]))))
+(t/deftest test-odd-number-repeated
+  (t/is (= 2.25 (statistics/median-grouped [1 2 2 3 4]))))
 
-(t/deftest ^:kaocha/skip test-even-number-repeated
-  (t/is (= 3.5 (statistics/median-grouped [1 2 3 3 4 5]))))
+(t/deftest test-even-number-repeated
+  (t/is (= 3.0 (statistics/median-grouped [1 2 3 3 4 5]))))
 
-(t/deftest ^:kaocha/skip test-mediangrouped-single-value
-  (t/is (= 1.5 (statistics/median-grouped [1])))
+(t/deftest test-mediangrouped-single-value
+  (t/is (= 1.0 (statistics/median-grouped [1])))
   (t/is (= 2.0 (statistics/median-grouped [2]))))
 
-(t/deftest ^:kaocha/skip test-interval
-  (t/is (= 3.25 (statistics/median-grouped [1 2 3 3 4 5] 2))))
+(t/deftest test-interval
+  (t/is (= 3.0 (statistics/median-grouped [1 2 3 3 4 5] 2))))
 
 ;; ---- mode ----
 
-(t/deftest ^:kaocha/skip test-mode-range-data
+(t/deftest test-mode-range-data
   (t/is (= 5 (statistics/mode [1 1 2 5 5 5 7]))))
 
-(t/deftest ^:kaocha/skip test-nominal-data
+(t/deftest test-nominal-data
   (t/is (= "red" (statistics/mode ["red" "blue" "red" "green"]))))
 
-(t/deftest ^:kaocha/skip test-discrete-data
+(t/deftest test-discrete-data
   (t/is (= 3/8 (statistics/mode [3/8 1/2 3/8 3/4]))))
 
-(t/deftest ^:kaocha/skip test-unique-data
+(t/deftest test-unique-data
   (t/is (= 3 (statistics/mode [3 1 2 4]))))
 
 ;; ---- multimode ----
 
-(t/deftest ^:kaocha/skip test-multimode-basics
+(t/deftest test-multimode-basics
   (t/is (= [1 3] (statistics/multimode [1 1 2 3 3])))
   (t/is (= [5] (statistics/multimode [1 1 2 5 5 5 7])))
   (t/is (= [] (statistics/multimode []))))
 
 ;; ---- pvariance ----
 
-(t/deftest ^:kaocha/skip test-pvariance-ints
+(t/deftest test-pvariance-ints
   (t/is (= 4 (statistics/pvariance [1 2 3 4 5 6 7]))))
 
-(t/deftest ^:kaocha/skip test-pvariance-fractions
-  (t/is (= 1/4 (statistics/pvariance [1/4 3/4]))))
+(t/deftest test-pvariance-fractions
+  (t/is (= 1/16 (statistics/pvariance [1/4 3/4]))))
 
-(t/deftest ^:kaocha/skip test-variancestdev-single-value
+(t/deftest test-variancestdev-single-value
   (t/is (= 0 (statistics/pvariance [17]))))
 
 ;; ---- variance ----
 
-(t/deftest ^:kaocha/skip test-variance-single-value
-  (t/is (= 35/4 (statistics/variance [1 2 3 4 5 6 7])))
+(t/deftest test-variance-single-value
+  (t/is (= 14/3 (statistics/variance [1 2 3 4 5 6 7])))
   (t/is (thrown? ExceptionInfo (statistics/variance [17]))))
 
-(t/deftest ^:kaocha/skip test-variance-ints
-  (t/is (= 35/4 (statistics/variance [1 2 3 4 5 6 7]))))
+(t/deftest test-variance-ints
+  (t/is (= 14/3 (statistics/variance [1 2 3 4 5 6 7]))))
 
-(t/deftest ^:kaocha/skip test-variance-fractions
-  (t/is (= 1/2 (statistics/variance [1/4 3/4]))))
+(t/deftest test-variance-fractions
+  (t/is (= 1/8 (statistics/variance [1/4 3/4]))))
 
 ;; ---- stdev ----
 
-(t/deftest ^:kaocha/skip test-stdev-single-value
+(t/deftest test-stdev-single-value
   (t/is (< (Math/abs (- (statistics/stdev [1 2 3])
                         (Math/sqrt (statistics/variance [1 2 3]))))
            1e-10))
   (t/is (thrown? ExceptionInfo (statistics/stdev [17]))))
 
-(t/deftest ^:kaocha/skip test-stdev-compare-to-variance
+(t/deftest test-stdev-compare-to-variance
   (let [data [1.0 2.0 3.0 4.0 5.0]]
     (t/is (< (Math/abs (- (statistics/stdev data)
                           (Math/sqrt (statistics/variance data))))
@@ -210,7 +209,7 @@
 
 ;; ---- pstdev ----
 
-(t/deftest ^:kaocha/skip test-pstdev-compare-to-variance
+(t/deftest test-pstdev-compare-to-variance
   (let [data [1.0 2.0 3.0 4.0 5.0]]
     (t/is (< (Math/abs (- (statistics/pstdev data)
                           (Math/sqrt (statistics/pvariance data))))
@@ -218,30 +217,30 @@
 
 ;; ---- quantiles ----
 
-(t/deftest ^:kaocha/skip test-specific-cases
+(t/deftest test-specific-cases
   (t/is (= [1.5 3.0 4.5] (statistics/quantiles [1 2 3 4 5])))
-  (t/is (= [1.0] (statistics/quantiles [1 2 3] {:n 2}))))
+  (t/is (= [2.0] (statistics/quantiles [1 2 3] {:n 2}))))
 
-(t/deftest ^:kaocha/skip test-quantiles-error-cases
-  (t/is (= [1.0] (statistics/quantiles [1 2 3] {:n 2})))
+(t/deftest test-quantiles-error-cases
+  (t/is (= [2.0] (statistics/quantiles [1 2 3] {:n 2})))
   (t/is (thrown? ExceptionInfo (statistics/quantiles [1 2] {:n 0})))
   (t/is (thrown? ExceptionInfo (statistics/quantiles [1]))))
 
 ;; ---- correlation and covariance ----
 
-(t/deftest ^:kaocha/skip test-correlationandcovariance-results
+(t/deftest test-correlationandcovariance-results
   (let [x [1 2 3 4 5]
         y [1 2 3 4 5]]
     (t/is (< (Math/abs (- 1.0 (statistics/correlation x y))) 1e-10))
     (t/is (< (Math/abs (- 2.5 (statistics/covariance x y))) 1e-10))))
 
-(t/deftest ^:kaocha/skip test-constant-input-error
+(t/deftest test-constant-input-error
   (t/is (= 1.0 (statistics/correlation [1 2 3] [1 2 3])))
   (t/is (thrown? ExceptionInfo (statistics/correlation [1 1 1] [1 2 3]))))
 
 ;; ---- linear-regression ----
 
-(t/deftest ^:kaocha/skip test-linearregression-results
+(t/deftest test-linearregression-results
   (let [result (statistics/linear-regression [1 2 3] [2 4 6])]
     (t/is (< (Math/abs (- 2.0 (:slope result))) 1e-10))
     (t/is (< (Math/abs (- 0.0 (:intercept result))) 1e-10))))
