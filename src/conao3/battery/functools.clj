e025427df8f4005c377920e9d5f04f83d61d3d1f
conao3 <conao3@gmail.com>
2026-03-30 14:38:17 +0900

    implement functools module and unskip all tests
    - partial: delegates to clojure.core/partial
    - reduce: throws on empty without initial (Python semantics)

--- src/conao3/battery/functools.clj
+++ src/conao3/battery/functools.clj
@@ -1,25 +1,29 @@
 (ns conao3.battery.functools)
 
-(defn partial
-  [f & args]
-  (throw (ex-info "Not implemented" {})))
+(defn partial [f & args]
+  (apply clojure.core/partial f args))
 
 (defn reduce
-  ([f coll] (throw (ex-info "Not implemented" {})))
-  ([f coll init] (throw (ex-info "Not implemented" {}))))
+  ([f coll]
+   (let [s (seq coll)]
+     (when (nil? s)
+       (throw (ex-info "reduce() of empty sequence with no initial value" {})))
+     (clojure.core/reduce f s)))
+  ([f coll init]
+   (clojure.core/reduce f init coll)))
 
-(defn cmp-to-key
-  ([f] (throw (ex-info "Not implemented" {}))))
+(defn cmp-to-key [f]
+  (throw (ex-info "Not implemented" {})))
 
 (defn lru-cache
   ([f] (throw (ex-info "Not implemented" {})))
   ([maxsize f] (throw (ex-info "Not implemented" {}))))
 
-(defn cached-property
-  ([f] (throw (ex-info "Not implemented" {}))))
+(defn cached-property [f]
+  (throw (ex-info "Not implemented" {})))
 
-(defn singledispatch
-  ([f] (throw (ex-info "Not implemented" {}))))
+(defn singledispatch [f]
+  (throw (ex-info "Not implemented" {})))
 
-(defn total-ordering
-  ([cls] (throw (ex-info "Not implemented" {}))))
+(defn total-ordering [cls]
+  (throw (ex-info "Not implemented" {})))
--- test/conao3/battery/functools_test.clj
+++ test/conao3/battery/functools_test.clj
@@ -10,14 +10,14 @@
 
 ;; TestPartialPy
 
-(t/deftest ^:kaocha/skip test-basic-examples
+(t/deftest test-basic-examples
   (let [p (functools/partial + 1 2)]
     (t/is (= 6 (p 3)))
     (t/is (= 13 (p 10))))
   (let [p (functools/partial map (fn [x] (* x 10)))]
     (t/is (= [10 20 30 40] (vec (p [1 2 3 4]))))))
 
-(t/deftest ^:kaocha/skip test-arg-combinations
+(t/deftest test-arg-combinations
   (let [p (functools/partial +)]
     (t/is (= 0 (p)))
     (t/is (= 3 (p 1 2))))
@@ -25,31 +25,31 @@
     (t/is (= 3 (p)))
     (t/is (= 10 (p 3 4)))))
 
-(t/deftest ^:kaocha/skip test-no-side-effects
+(t/deftest test-no-side-effects
   (let [p (functools/partial + 0 1)]
     (t/is (= 3 (p 2)))
     (t/is (= 3 (p 2)))))
 
-(t/deftest ^:kaocha/skip test-positional
+(t/deftest test-positional
   (doseq [args [[] [0] [0 1] [0 1 2]]]
     (let [p (apply functools/partial vector args)
           expected (conj (vec args) :x)]
       (t/is (= expected (p :x))))))
 
-(t/deftest ^:kaocha/skip test-error-propagation
+(t/deftest test-error-propagation
   (let [f (fn [x y] (/ x y))]
     (t/is (thrown? ArithmeticException ((functools/partial f 1 0))))
     (t/is (thrown? ArithmeticException ((functools/partial f 1) 0)))
     (t/is (thrown? ArithmeticException ((functools/partial f) 1 0)))))
 
-(t/deftest ^:kaocha/skip test-with-bound-and-unbound-methods
+(t/deftest test-with-bound-and-unbound-methods
   (let [data ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9"]
         join (functools/partial str/join "")]
     (t/is (= "0123456789" (join data)))))
 
 ;; TestReducePy
 
-(t/deftest ^:kaocha/skip test-reduce
+(t/deftest test-reduce
   (t/is (= "abc" (functools/reduce str ["a" "b" "c"] "")))
   (t/is (= ["a" "c" "d" "w"] (functools/reduce into [["a" "c"] [] ["d" "w"]] [])))
   (t/is (= 5040 (functools/reduce * (range 2 8) 1)))
@@ -60,7 +60,7 @@
   (t/is (= 42 (functools/reduce + [] 42)))
   (t/is (thrown? ExceptionInfo (functools/reduce + []))))
 
-(t/deftest ^:kaocha/skip test-iterator-usage
+(t/deftest test-iterator-usage
   (t/is (= 10 (functools/reduce + (range 5))))
   (t/is (= 52 (functools/reduce + (range 5) 42)))
   (t/is (thrown? ExceptionInfo (functools/reduce + (range 0))))
