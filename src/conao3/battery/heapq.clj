363d931e94331e3e8f08836c61578cbe0887a048
conao3 <conao3@gmail.com>
2026-03-30 14:44:03 +0900

    implement heapq module and unskip all tests
    - min/max heap operations: heappush!, heappop!, heapify!, heapreplace!,
      heappushpop!, and max variants
    - nsmallest, nlargest, heap-merge with :key support
    - wrap comparison exceptions in ExceptionInfo via safe-compare
    - fix test bugs: nsmallest/nlargest sanity-check expected values,
      test-nworst-minheap reverse assertion

--- src/conao3/battery/heapq.clj
+++ src/conao3/battery/heapq.clj
@@ -1,67 +1,167 @@
 (ns conao3.battery.heapq)
 
-(defn heappush!
-  "Push item onto heap (atom of vector), maintaining heap invariant."
-  [heap-atom item]
-  (throw (ex-info "Not implemented" {})))
+(defn- validate-heap [heap-atom]
+  (when-not (instance? clojure.lang.IAtom heap-atom)
+    (throw (ex-info "heap must be an atom" {})))
+  (when-not (vector? @heap-atom)
+    (throw (ex-info "heap must contain a vector" {}))))
 
-(defn heappop!
-  "Pop and return smallest item from heap (atom of vector)."
-  [heap-atom]
-  (throw (ex-info "Not implemented" {})))
+(defn- safe-compare [a b]
+  (try
+    (compare a b)
+    (catch Throwable e
+      (throw (ex-info "comparison failed" {} e)))))
 
-(defn heapify!
-  "Transform vector in-place (atom of vector) into a heap."
-  [heap-atom]
-  (throw (ex-info "Not implemented" {})))
+(defn- sift-up [v i]
+  (loop [v v i i]
+    (let [parent (quot (dec i) 2)]
+      (if (and (pos? i) (neg? (safe-compare (nth v i) (nth v parent))))
+        (recur (assoc v i (nth v parent) parent (nth v i)) parent)
+        v))))
 
-(defn heapreplace!
-  "Pop and return smallest item, then push item. heap must not be empty."
-  [heap-atom item]
-  (throw (ex-info "Not implemented" {})))
+(defn- sift-down [v i n]
+  (loop [v v i i]
+    (let [left (inc (* 2 i))
+          right (+ 2 (* 2 i))
+          smallest (if (and (< left n) (neg? (safe-compare (nth v left) (nth v i))))
+                     left i)
+          smallest (if (and (< right n) (neg? (safe-compare (nth v right) (nth v smallest))))
+                     right smallest)]
+      (if (= smallest i)
+        v
+        (recur (assoc v i (nth v smallest) smallest (nth v i)) smallest)))))
 
-(defn heappushpop!
-  "Fast version of heappush followed by heappop."
-  [heap-atom item]
-  (throw (ex-info "Not implemented" {})))
+(defn- sift-up-max [v i]
+  (loop [v v i i]
+    (let [parent (quot (dec i) 2)]
+      (if (and (pos? i) (pos? (safe-compare (nth v i) (nth v parent))))
+        (recur (assoc v i (nth v parent) parent (nth v i)) parent)
+        v))))
 
-(defn heappush-max!
-  "Push item onto max-heap (atom of vector), maintaining max-heap invariant."
-  [heap-atom item]
-  (throw (ex-info "Not implemented" {})))
+(defn- sift-down-max [v i n]
+  (loop [v v i i]
+    (let [left (inc (* 2 i))
+          right (+ 2 (* 2 i))
+          largest (if (and (< left n) (pos? (safe-compare (nth v left) (nth v i))))
+                    left i)
+          largest (if (and (< right n) (pos? (safe-compare (nth v right) (nth v largest))))
+                   right largest)]
+      (if (= largest i)
+        v
+        (recur (assoc v i (nth v largest) largest (nth v i)) largest)))))
 
-(defn heappop-max!
-  "Pop and return largest item from max-heap (atom of vector)."
-  [heap-atom]
-  (throw (ex-info "Not implemented" {})))
+(defn heappush! [heap-atom item]
+  (validate-heap heap-atom)
+  (swap! heap-atom (fn [v]
+    (let [v (conj v item)]
+      (sift-up v (dec (count v)))))))
 
-(defn heapify-max!
-  "Transform vector in-place (atom of vector) into a max-heap."
-  [heap-atom]
-  (throw (ex-info "Not implemented" {})))
+(defn heappop! [heap-atom]
+  (validate-heap heap-atom)
+  (let [v @heap-atom
+        n (count v)]
+    (when (zero? n)
+      (throw (ex-info "heap is empty" {})))
+    (let [top (nth v 0)]
+      (if (= n 1)
+        (reset! heap-atom [])
+        (let [last-item (nth v (dec n))
+              v (subvec v 0 (dec n))]
+          (reset! heap-atom (sift-down (assoc v 0 last-item) 0 (dec n)))))
+      top)))
 
-(defn heapreplace-max!
-  "Pop and return largest item from max-heap, then push item."
-  [heap-atom item]
-  (throw (ex-info "Not implemented" {})))
+(defn heapify! [heap-atom]
+  (validate-heap heap-atom)
+  (let [v @heap-atom
+        n (count v)]
+    (reset! heap-atom
+            (reduce (fn [v i] (sift-down v i n))
+                    v
+                    (range (dec (quot n 2)) -1 -1)))))
 
-(defn heappushpop-max!
-  "Fast version of heappush-max! followed by heappop-max!."
-  [heap-atom item]
-  (throw (ex-info "Not implemented" {})))
+(defn heapreplace! [heap-atom item]
+  (validate-heap heap-atom)
+  (let [v @heap-atom]
+    (when (empty? v)
+      (throw (ex-info "heap is empty" {})))
+    (let [top (nth v 0)
+          v (assoc v 0 item)]
+      (reset! heap-atom (sift-down v 0 (count v)))
+      top)))
 
-(defn nsmallest
-  "Return a vector with the n smallest elements from data."
-  [n data]
-  (throw (ex-info "Not implemented" {})))
+(defn heappushpop! [heap-atom item]
+  (validate-heap heap-atom)
+  (let [v @heap-atom]
+    (if (or (empty? v) (not (pos? (safe-compare item (nth v 0)))))
+      item
+      (let [top (nth v 0)
+            v (assoc v 0 item)]
+        (reset! heap-atom (sift-down v 0 (count v)))
+        top))))
 
-(defn nlargest
-  "Return a vector with the n largest elements from data."
-  [n data]
-  (throw (ex-info "Not implemented" {})))
+(defn heappush-max! [heap-atom item]
+  (validate-heap heap-atom)
+  (swap! heap-atom (fn [v]
+    (let [v (conj v item)]
+      (sift-up-max v (dec (count v)))))))
 
-(defn heap-merge
-  "Merge sorted inputs into a single sorted output (lazy seq).
-  Options: :key fn, :reverse bool"
-  [& args]
-  (throw (ex-info "Not implemented" {})))
+(defn heappop-max! [heap-atom]
+  (validate-heap heap-atom)
+  (let [v @heap-atom
+        n (count v)]
+    (when (zero? n)
+      (throw (ex-info "heap is empty" {})))
+    (let [top (nth v 0)]
+      (if (= n 1)
+        (reset! heap-atom [])
+        (let [last-item (nth v (dec n))
+              v (subvec v 0 (dec n))]
+          (reset! heap-atom (sift-down-max (assoc v 0 last-item) 0 (dec n)))))
+      top)))
+
+(defn heapify-max! [heap-atom]
+  (validate-heap heap-atom)
+  (let [v @heap-atom
+        n (count v)]
+    (reset! heap-atom
+            (reduce (fn [v i] (sift-down-max v i n))
+                    v
+                    (range (dec (quot n 2)) -1 -1)))))
+
+(defn heapreplace-max! [heap-atom item]
+  (validate-heap heap-atom)
+  (let [v @heap-atom]
+    (when (empty? v)
+      (throw (ex-info "heap is empty" {})))
+    (let [top (nth v 0)
+          v (assoc v 0 item)]
+      (reset! heap-atom (sift-down-max v 0 (count v)))
+      top)))
+
+(defn heappushpop-max! [heap-atom item]
+  (validate-heap heap-atom)
+  (let [v @heap-atom]
+    (if (or (empty? v) (not (neg? (safe-compare item (nth v 0)))))
+      item
+      (let [top (nth v 0)
+            v (assoc v 0 item)]
+        (reset! heap-atom (sift-down-max v 0 (count v)))
+        top))))
+
+(defn nsmallest [n data]
+  (when-not (sequential? data)
+    (throw (ex-info "data must be iterable" {})))
+  (vec (take n (sort data))))
+
+(defn nlargest [n data]
+  (when-not (sequential? data)
+    (throw (ex-info "data must be iterable" {})))
+  (vec (take n (sort > data))))
+
+(defn heap-merge [& args]
+  (let [n (count args)
+        has-key? (and (>= n 2) (= :key (nth args (- n 2))))
+        [seqs key-fn] (if has-key?
+                        [(take (- n 2) args) (last args)]
+                        [args identity])]
+    (sort-by key-fn (apply concat seqs))))
--- test/conao3/battery/heapq_test.clj
+++ test/conao3/battery/heapq_test.clj
@@ -11,7 +11,7 @@
 ;; - TestModules.test_c_functions / load_tests (doctest registration)
 ;; - TestHeapC / TestErrorHandlingC (@skipUnless(c_heapq))
 
-(t/deftest ^:kaocha/skip test-py-functions
+(t/deftest test-py-functions
   (doseq [f '[heappush! heappop! heappush-max! heappop-max!
               heapify! heapify-max! heapreplace! heapreplace-max!
               heappushpop! heappushpop-max! nsmallest nlargest heap-merge]]
@@ -19,7 +19,7 @@
 
 ;; ---- TestHeapPython (= TestHeap) ----
 
-(t/deftest ^:kaocha/skip test-push-pop
+(t/deftest test-push-pop
   (let [heap (atom [])]
     (heapq/heappush! heap 3)
     (heapq/heappush! heap 1)
@@ -30,7 +30,7 @@
   (t/is (thrown? ExceptionInfo (heapq/heappush! nil 1)))
   (t/is (thrown? ExceptionInfo (heapq/heappop! nil))))
 
-(t/deftest ^:kaocha/skip test-max-push-pop
+(t/deftest test-max-push-pop
   (let [heap (atom [])]
     (heapq/heappush-max! heap 1)
     (heapq/heappush-max! heap 3)
@@ -40,7 +40,7 @@
     (t/is (= 1 (heapq/heappop-max! heap))))
   (t/is (thrown? ExceptionInfo (heapq/heappush-max! nil 1))))
 
-(t/deftest ^:kaocha/skip test-heapify
+(t/deftest test-heapify
   (let [heap (atom [3 1 4 1 5 9 2 6])]
     (heapq/heapify! heap)
     (let [results (loop [acc []]
@@ -50,7 +50,7 @@
       (t/is (= [1 1 2 3 4 5 6 9] results))))
   (t/is (thrown? ExceptionInfo (heapq/heapify! nil))))
 
-(t/deftest ^:kaocha/skip test-heapify-max
+(t/deftest test-heapify-max
   (let [heap (atom [3 1 4 1 5 9 2 6])]
     (heapq/heapify-max! heap)
     (let [results (loop [acc []]
@@ -60,7 +60,7 @@
       (t/is (= [9 6 5 4 3 2 1 1] results))))
   (t/is (thrown? ExceptionInfo (heapq/heapify-max! nil))))
 
-(t/deftest ^:kaocha/skip test-naive-nbest
+(t/deftest test-naive-nbest
   (let [data [5 3 8 1 9 2 7 4 6 0 12 11 10]
         heap (atom [])]
     (doseq [item data]
@@ -69,7 +69,7 @@
         (heapq/heappop! heap)))
     (t/is (= (sort (take-last 4 (sort data))) (sort @heap)))))
 
-(t/deftest ^:kaocha/skip test-nbest
+(t/deftest test-nbest
   (let [data [5 3 8 1 9 2 7 4 6 0]
         heap (atom (vec (take 3 data)))]
     (heapq/heapify! heap)
@@ -80,14 +80,14 @@
   (t/is (thrown? ExceptionInfo (heapq/heapreplace! (atom nil) 1)))
   (t/is (thrown? ExceptionInfo (heapq/heapreplace! (atom []) 1))))
 
-(t/deftest ^:kaocha/skip test-nbest-maxheap
+(t/deftest test-nbest-maxheap
   (let [data [5 3 8 1 9 2 7 4 6 0]
         heap (atom (vec data))]
     (heapq/heapify-max! heap)
     (let [result (vec (repeatedly 3 #(heapq/heappop-max! heap)))]
       (t/is (= (reverse (take-last 3 (sort data))) result)))))
 
-(t/deftest ^:kaocha/skip test-nbest-with-pushpop
+(t/deftest test-nbest-with-pushpop
   (let [data [5 3 8 1 9 2 7 4 6 0]
         heap (atom (vec (take 3 data)))]
     (heapq/heapify! heap)
@@ -96,7 +96,7 @@
     (t/is (= (sort (take-last 3 (sort data))) (sort @heap))))
   (t/is (= "x" (heapq/heappushpop! (atom []) "x"))))
 
-(t/deftest ^:kaocha/skip test-naive-nworst
+(t/deftest test-naive-nworst
   (let [data [5 3 8 1 9 2 7 4 6 0]
         heap (atom [])]
     (doseq [item data]
@@ -105,7 +105,7 @@
         (heapq/heappop-max! heap)))
     (t/is (= (sort (take 3 (sort data))) (sort @heap)))))
 
-(t/deftest ^:kaocha/skip test-nworst
+(t/deftest test-nworst
   (let [data [5 3 8 1 9 2 7 4 6 0]
         heap (atom (vec (take 3 data)))]
     (heapq/heapify-max! heap)
@@ -116,14 +116,14 @@
   (t/is (thrown? ExceptionInfo (heapq/heapreplace-max! (atom nil) 1)))
   (t/is (thrown? ExceptionInfo (heapq/heapreplace-max! (atom []) 1))))
 
-(t/deftest ^:kaocha/skip test-nworst-minheap
+(t/deftest test-nworst-minheap
   (let [data [5 3 8 1 9 2 7 4 6 0]
         heap (atom (vec data))]
     (heapq/heapify! heap)
     (let [result (vec (repeatedly 3 #(heapq/heappop! heap)))]
-      (t/is (= (take 3 (sort data)) (reverse result))))))
+      (t/is (= (take 3 (sort data)) result)))))
 
-(t/deftest ^:kaocha/skip test-nworst-with-pushpop
+(t/deftest test-nworst-with-pushpop
   (let [data [5 3 8 1 9 2 7 4 6 0]
         heap (atom (vec (take 3 data)))]
     (heapq/heapify-max! heap)
@@ -132,7 +132,7 @@
     (t/is (= (take 3 (sort data)) (sort @heap))))
   (t/is (= "x" (heapq/heappushpop-max! (atom []) "x"))))
 
-(t/deftest ^:kaocha/skip test-heappushpop
+(t/deftest test-heappushpop
   (let [heap (atom [])]
     (t/is (= 10 (heapq/heappushpop! heap 10)))
     (t/is (empty? @heap)))
@@ -143,7 +143,7 @@
     (t/is (= 10 (heapq/heappushpop! heap 11)))
     (t/is (= [11] @heap))))
 
-(t/deftest ^:kaocha/skip test-heappushpop-max
+(t/deftest test-heappushpop-max
   (let [heap (atom [])]
     (t/is (= 10 (heapq/heappushpop-max! heap 10)))
     (t/is (empty? @heap)))
@@ -154,12 +154,12 @@
     (t/is (= 10 (heapq/heappushpop-max! heap 9)))
     (t/is (= [9] @heap))))
 
-(t/deftest ^:kaocha/skip test-heappop-max
+(t/deftest test-heappop-max
   (let [heap (atom [3 2])]
     (t/is (= 3 (heapq/heappop-max! heap)))
     (t/is (= 2 (heapq/heappop-max! heap)))))
 
-(t/deftest ^:kaocha/skip test-heapsort
+(t/deftest test-heapsort
   (let [data [3 1 4 1 5 9 2 6]
         heap (atom (vec data))]
     (heapq/heapify! heap)
@@ -169,7 +169,7 @@
                       (recur (conj acc (heapq/heappop! heap)))))]
       (t/is (= (sort data) results)))))
 
-(t/deftest ^:kaocha/skip test-heapsort-max
+(t/deftest test-heapsort-max
   (let [data [3 1 4 1 5 9 2 6]
         heap (atom (vec data))]
     (heapq/heapify-max! heap)
@@ -179,21 +179,21 @@
                       (recur (conj acc (heapq/heappop-max! heap)))))]
       (t/is (= (sort > data) results)))))
 
-(t/deftest ^:kaocha/skip test-merge
+(t/deftest test-merge
   (t/is (= [1 2 3 4 5 6] (vec (heapq/heap-merge [1 3 5] [2 4 6]))))
   (t/is (= [1 2 3] (vec (heapq/heap-merge [1] [2] [3]))))
   (t/is (= [] (vec (heapq/heap-merge)))))
 
-(t/deftest ^:kaocha/skip test-empty-merges
+(t/deftest test-empty-merges
   (t/is (= [] (vec (heapq/heap-merge [] []))))
   (t/is (= [] (vec (heapq/heap-merge [] [] :key identity)))))
 
-(t/deftest ^:kaocha/skip test-merge-does-not-suppress-index-error
+(t/deftest test-merge-does-not-suppress-index-error
   (t/is (thrown? ExceptionInfo
                  (let [bad-seq (concat [1 2] (lazy-seq (throw (ExceptionInfo. "boom" {}))))]
                    (vec (heapq/heap-merge bad-seq))))))
 
-(t/deftest ^:kaocha/skip test-merge-stability
+(t/deftest test-merge-stability
   (let [tagged (fn [src xs] (map #(hash-map :v % :src src) xs))
         s1 (tagged 0 [1 3 5])
         s2 (tagged 1 [1 3 5])
@@ -201,17 +201,17 @@
     (t/is (= (map :v result) (sort (map :v (concat s1 s2)))))
     (t/is (= 0 (:src (first result))))))
 
-(t/deftest ^:kaocha/skip test-nsmallest
+(t/deftest test-nsmallest
   (t/is (= [1 2 3] (heapq/nsmallest 3 [5 1 4 2 3])))
   (t/is (= [] (heapq/nsmallest 0 [5 1 4 2 3])))
   (t/is (= [1] (heapq/nsmallest 1 [3 1 2]))))
 
-(t/deftest ^:kaocha/skip test-nlargest
+(t/deftest test-nlargest
   (t/is (= [5 4 3] (heapq/nlargest 3 [5 1 4 2 3])))
   (t/is (= [] (heapq/nlargest 0 [5 1 4 2 3])))
   (t/is (= [3] (heapq/nlargest 1 [3 1 2]))))
 
-(t/deftest ^:kaocha/skip test-comparison-operator
+(t/deftest test-comparison-operator
   (let [data [3 1 4 1 5 9 2 6]
         heap (atom [])]
     (doseq [item data]
@@ -224,8 +224,8 @@
 
 ;; ---- TestErrorHandlingPython (= TestErrorHandling) ----
 
-(t/deftest ^:kaocha/skip test-non-sequence
-  (t/is (= [2 3 4] (heapq/nsmallest 2 [2 3 4 5])))
+(t/deftest test-non-sequence
+  (t/is (= [2 3] (heapq/nsmallest 2 [2 3 4 5])))
   (t/is (thrown? ExceptionInfo (heapq/heapify! (atom 10))))
   (t/is (thrown? ExceptionInfo (heapq/heappop! (atom 10))))
   (t/is (thrown? ExceptionInfo (heapq/heappush! (atom 10) 1)))
@@ -233,22 +233,22 @@
   (t/is (thrown? ExceptionInfo (heapq/nlargest 2 10)))
   (t/is (thrown? ExceptionInfo (heapq/nsmallest 2 10))))
 
-(t/deftest ^:kaocha/skip test-len-only
-  (t/is (= [4 5 6] (heapq/nsmallest 2 [4 5 6 7])))
+(t/deftest test-len-only
+  (t/is (= [4 5] (heapq/nsmallest 2 [4 5 6 7])))
   (t/is (thrown? ExceptionInfo (heapq/heapify! (atom {:len 10}))))
   (t/is (thrown? ExceptionInfo (heapq/heappop! (atom {:len 10}))))
   (t/is (thrown? ExceptionInfo (heapq/heappush! (atom {:len 10}) 1)))
   (t/is (thrown? ExceptionInfo (heapq/heapreplace! (atom {:len 10}) 1))))
 
-(t/deftest ^:kaocha/skip test-cmp-err
+(t/deftest test-cmp-err
   (t/is (= [1 2] (heapq/nsmallest 2 [2 1 3 4])))
   (t/is (thrown? ExceptionInfo
                  (let [heap (atom [1 2 3])]
                    (heapq/heappush! heap (reify Comparable
                                            (compareTo [_ _] (throw (ArithmeticException. "/")))))))))
 
-(t/deftest ^:kaocha/skip test-arg-parsing
-  (t/is (= [7 8 9] (heapq/nlargest 2 [7 8 9 6])))
+(t/deftest test-arg-parsing
+  (t/is (= [9 8] (heapq/nlargest 2 [7 8 9 6])))
   (t/is (thrown? ExceptionInfo (heapq/heapify! 10)))
   (t/is (thrown? ExceptionInfo (heapq/heappop! 10)))
   (t/is (thrown? ExceptionInfo (heapq/heappush! 10 1)))
@@ -256,14 +256,14 @@
   (t/is (thrown? ExceptionInfo (heapq/nlargest 2 10)))
   (t/is (thrown? ExceptionInfo (heapq/nsmallest 2 10))))
 
-(t/deftest ^:kaocha/skip test-iterable-args
+(t/deftest test-iterable-args
   (t/is (= [1 2] (heapq/nsmallest 2 [3 1 2 4])))
   (t/is (= [4 3] (heapq/nlargest 2 [3 1 2 4])))
   (t/is (= [] (heapq/nsmallest 2 [])))
   (t/is (= [] (heapq/nlargest 2 []))))
 
-(t/deftest ^:kaocha/skip test-heappush-mutating-heap
-  (t/is (= [1 2] (heapq/nlargest 1 [1 2 3])))
+(t/deftest test-heappush-mutating-heap
+  (t/is (= [3] (heapq/nlargest 1 [1 2 3])))
   (t/is (thrown? ExceptionInfo
                  (let [heap (atom (vec (range 10)))]
                    (heapq/heapify! heap)
@@ -272,7 +272,7 @@
                                              (reset! heap [])
                                              (throw (RuntimeException. "heap modified")))))))))
 
-(t/deftest ^:kaocha/skip test-heappop-mutating-heap
+(t/deftest test-heappop-mutating-heap
   (t/is (= [1 2] (heapq/nsmallest 2 [1 2 3])))
   (t/is (thrown? ExceptionInfo
                  (let [heap (atom (vec (range 10)))]
@@ -283,7 +283,7 @@
                                              (throw (RuntimeException. "heap modified")))))
                    (heapq/heappop! heap)))))
 
-(t/deftest ^:kaocha/skip test-comparison-operator-modifying-heap
+(t/deftest test-comparison-operator-modifying-heap
   (t/is (= [0 1] (heapq/nsmallest 2 [0 1 2])))
   (t/is (thrown? ExceptionInfo
                  (let [heap (atom [0])
@@ -293,7 +293,7 @@
                                 (throw (RuntimeException. "heap modified"))))]
                   (heapq/heappushpop! heap evil)))))
 
-(t/deftest ^:kaocha/skip test-comparison-operator-modifying-heap-two-heaps
+(t/deftest test-comparison-operator-modifying-heap-two-heaps
   (t/is (= [9 8] (heapq/nlargest 2 [8 9 7 6])))
   (t/is (thrown? ExceptionInfo
                  (let [heap1 (atom [0])
