9cde6ca5eb59f090c7095ce6163a7fe7087ec30b
conao3 <conao3@gmail.com>
2026-03-31 13:26:49 +0900

    implement operator module and unskip all 12 tests

---
+++ src/conao3/battery/operator.clj
@@ -0,0 +1,128 @@
+;; Original: Lib/operator.py
+
+(ns conao3.battery.operator)
+
+(defn add [a b] (+ a b))
+(defn sub [a b] (- a b))
+(defn mul [a b] (* a b))
+(defn truediv [a b] (/ (double a) (double b)))
+(defn floordiv [a b] (Math/floorDiv (long a) (long b)))
+(defn mod [a b] (clojure.core/mod a b))
+(defn pow [a b] (Math/pow (double a) (double b)))
+(defn neg [a] (- a))
+(defn pos [a] (+ a))
+(defn abs [a] (Math/abs (double a)))
+(defn inv [a] (bit-not (long a)))
+(defn invert [a] (bit-not (long a)))
+
+(defn lshift [a b] (bit-shift-left (long a) (long b)))
+(defn rshift [a b] (bit-shift-right (long a) (long b)))
+(defn and_ [a b] (bit-and (long a) (long b)))
+(defn or_ [a b] (bit-or (long a) (long b)))
+(defn xor [a b] (bit-xor (long a) (long b)))
+
+(defn lt [a b] (< a b))
+(defn le [a b] (<= a b))
+(defn eq [a b] (= a b))
+(defn ne [a b] (not= a b))
+(defn ge [a b] (>= a b))
+(defn gt [a b] (> a b))
+
+(defn not_ [a] (not a))
+(defn truth [a] (boolean a))
+(defn is_ [a b] (identical? a b))
+(defn is-not [a b] (not (identical? a b)))
+
+(defn contains [a b]
+  (cond
+    (string? a) (.contains ^String a (str b))
+    (map? a) (contains? a b)
+    (set? a) (contains? a b)
+    (sequential? a) (boolean (some #(= % b) a))
+    :else (throw (ex-info (str "cannot check containment for " (type a)) {}))))
+
+(defn getitem [a b]
+  (cond
+    (string? a) (.charAt ^String a (int b))
+    (map? a) (let [v (get a b ::missing)]
+               (if (= v ::missing)
+                 (throw (ex-info (str "Key not found: " b) {:key b}))
+                 v))
+    (sequential? a) (nth a b)
+    (instance? java.util.List a) (.get ^java.util.List a (int b))
+    (instance? java.util.Map a) (let [v (.get ^java.util.Map a b)]
+                                   (when (and (nil? v) (not (.containsKey ^java.util.Map a b)))
+                                     (throw (ex-info (str "Key not found: " b) {:key b})))
+                                   v)
+    :else (throw (ex-info (str "cannot index " (type a)) {}))))
+
+(defn setitem [a b c]
+  (cond
+    (instance? java.util.List a) (.set ^java.util.List a (int b) c)
+    (instance? java.util.Map a) (.put ^java.util.Map a b c)
+    :else (throw (ex-info (str "cannot set item on " (type a)) {}))))
+
+(defn delitem [a b]
+  (cond
+    (instance? java.util.List a) (.remove ^java.util.List a (int b))
+    (instance? java.util.Map a) (.remove ^java.util.Map a b)
+    :else (throw (ex-info (str "cannot delete item from " (type a)) {}))))
+
+(defn length-hint [a]
+  (cond
+    (nil? a) 0
+    (string? a) (.length ^String a)
+    (counted? a) (count a)
+    (instance? java.util.Collection a) (.size ^java.util.Collection a)
+    :else 0))
+
+(defn concat [a b]
+  (cond
+    (string? a) (str a b)
+    (vector? a) (into a b)
+    (list? a) (apply list (clojure.core/concat a b))
+    (sequential? a) (clojure.core/concat a b)
+    :else (throw (ex-info (str "cannot concat " (type a)) {}))))
+
+(defn itemgetter
+  [& items]
+  (if (= 1 (count items))
+    (fn [obj] (getitem obj (first items)))
+    (fn [obj] (mapv #(getitem obj %) items))))
+
+(defn attrgetter
+  [& attrs]
+  (letfn [(get-attr [obj attr]
+            (let [parts (clojure.string/split attr #"\.")
+                  k (first parts)]
+              (let [v (cond
+                        (map? obj) (or (get obj k)
+                                       (get obj (keyword k))
+                                       (throw (ex-info (str "Attribute not found: " k) {:attr k})))
+                        :else (throw (ex-info (str "Cannot get attribute on " (type obj)) {})))]
+                (if (seq (rest parts))
+                  (get-attr v (clojure.string/join "." (rest parts)))
+                  v))))]
+    (if (= 1 (count attrs))
+      (fn [obj] (get-attr obj (first attrs)))
+      (fn [obj] (mapv #(get-attr obj %) attrs)))))
+
+(defn methodcaller
+  [name & args]
+  (fn [obj]
+    (let [m (.. obj getClass (getMethod name (into-array Class (map type args))))]
+      (.invoke m obj (into-array Object args)))))
+
+(defn iadd [a b] (add a b))
+(defn isub [a b] (sub a b))
+(defn imul [a b] (mul a b))
+(defn itruediv [a b] (truediv a b))
+(defn ifloordiv [a b] (floordiv a b))
+(defn imod [a b] (mod a b))
+(defn ipow [a b] (pow a b))
+(defn ilshift [a b] (lshift a b))
+(defn irshift [a b] (rshift a b))
+(defn iand [a b] (and_ a b))
+(defn ior [a b] (or_ a b))
+(defn ixor [a b] (xor a b))
+(defn iconcat [a b] (concat a b))
---
+++ test/conao3/battery/operator_test.clj
@@ -0,0 +1,106 @@
+;; Original: Lib/test/test_operator.py
+
+(ns conao3.battery.operator-test
+  (:require
+   [clojure.test :as t]
+   [conao3.battery.operator :as op])
+  (:import
+   [clojure.lang ExceptionInfo]
+   [java.util ArrayList HashMap]))
+
+(t/deftest test-arithmetic
+  (t/is (= 5 (op/add 2 3)))
+  (t/is (= -1 (op/sub 2 3)))
+  (t/is (= 6 (op/mul 2 3)))
+  (t/is (= 2.0 (op/truediv 4 2)))
+  (t/is (= 1 (op/floordiv 7 4)))
+  (t/is (= 1 (op/mod 7 3)))
+  (t/is (= 8.0 (op/pow 2 3)))
+  (t/is (= -5 (op/neg 5)))
+  (t/is (= 5 (op/pos 5)))
+  (t/is (= 5.0 (op/abs -5)))
+  (t/is (= 5.0 (op/abs 5))))
+
+(t/deftest test-bitwise
+  (t/is (= 4 (op/lshift 1 2)))
+  (t/is (= 1 (op/rshift 4 2)))
+  (t/is (= 0 (op/and_ 3 4)))
+  (t/is (= 7 (op/or_ 3 4)))
+  (t/is (= 7 (op/xor 3 4)))
+  (t/is (= -4 (op/inv 3)))
+  (t/is (= -4 (op/invert 3))))
+
+(t/deftest test-comparisons
+  (t/is (true? (op/lt 1 2)))
+  (t/is (false? (op/lt 2 1)))
+  (t/is (true? (op/le 1 1)))
+  (t/is (true? (op/eq 1 1)))
+  (t/is (true? (op/ne 1 2)))
+  (t/is (true? (op/ge 2 1)))
+  (t/is (true? (op/gt 2 1))))
+
+(t/deftest test-logical
+  (t/is (false? (op/not_ true)))
+  (t/is (true? (op/not_ false)))
+  (t/is (true? (op/truth 1)))
+  (t/is (false? (op/truth nil)))
+  (t/is (true? (let [x :a] (op/is_ x x))))
+  (t/is (false? (op/is_ :a :b)))
+  (t/is (false? (let [x :a] (op/is-not x x))))
+  (t/is (true? (op/is-not :a :b))))
+
+(t/deftest test-contains
+  (t/is (true? (op/contains [1 2 3] 2)))
+  (t/is (false? (op/contains [1 2 3] 5)))
+  (t/is (true? (op/contains #{:a :b} :a)))
+  (t/is (true? (op/contains {:a 1} :a)))
+  (t/is (true? (op/contains "hello" "ell")))
+  (t/is (false? (op/contains "hello" "xyz"))))
+
+(t/deftest test-getitem
+  (t/is (= 2 (op/getitem [1 2 3] 1)))
+  (t/is (= 1 (op/getitem {:a 1} :a)))
+  (t/is (= \e (op/getitem "hello" 1)))
+  (t/is (thrown? ExceptionInfo (op/getitem {:a 1} :b)))
+  (t/is (thrown? ExceptionInfo (op/getitem 42 0))))
+
+(t/deftest test-setitem-delitem
+  (let [lst (ArrayList. [1 2 3])]
+    (op/setitem lst 1 99)
+    (t/is (= [1 99 3] (vec lst)))
+    (op/delitem lst 0)
+    (t/is (= [99 3] (vec lst))))
+  (let [m (HashMap. {"a" 1})]
+    (op/setitem m "b" 2)
+    (t/is (= {"a" 1 "b" 2} (into {} m)))
+    (op/delitem m "a")
+    (t/is (= {"b" 2} (into {} m)))))
+
+(t/deftest test-length-hint
+  (t/is (= 3 (op/length-hint [1 2 3])))
+  (t/is (= 5 (op/length-hint "hello")))
+  (t/is (= 2 (op/length-hint {:a 1 :b 2})))
+  (t/is (= 0 (op/length-hint nil))))
+
+(t/deftest test-concat
+  (t/is (= "hello world" (op/concat "hello " "world")))
+  (t/is (= [1 2 3 4] (op/concat [1 2] [3 4])))
+  (t/is (= '(1 2 3 4) (op/concat '(1 2) '(3 4)))))
+
+(t/deftest test-itemgetter
+  (let [g (op/itemgetter 1)]
+    (t/is (= 2 (g [1 2 3]))))
+  (let [g (op/itemgetter 1 2)]
+    (t/is (= [2 3] (g [1 2 3])))))
+
+(t/deftest test-attrgetter
+  (let [g (op/attrgetter "name")]
+    (t/is (= "alice" (g {"name" "alice"}))))
+  (let [g (op/attrgetter "a" "b")]
+    (t/is (= ["x" "y"] (g {"a" "x" "b" "y"})))))
+
+(t/deftest test-in-place
+  (t/is (= 5 (op/iadd 2 3)))
+  (t/is (= -1 (op/isub 2 3)))
+  (t/is (= 6 (op/imul 2 3)))
+  (t/is (= "hello world" (op/iconcat "hello " "world"))))
