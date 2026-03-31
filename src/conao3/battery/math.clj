95d6aa59445b54cbc71634bb213b8a08e582fffc
conao3 <conao3@gmail.com>
2026-03-31 13:13:20 +0900

    implement math module and unskip all 18 tests

---
+++ src/conao3/battery/math.clj
@@ -0,0 +1,129 @@
+(ns conao3.battery.math)
+
+(def pi Math/PI)
+(def e Math/E)
+(def tau (* 2.0 Math/PI))
+(def inf Double/POSITIVE_INFINITY)
+(def nan Double/NaN)
+
+(defn sqrt [x] (Math/sqrt (double x)))
+(defn floor [x] (long (Math/floor (double x))))
+(defn ceil [x] (long (Math/ceil (double x))))
+(defn trunc [x] (long x))
+(defn fabs [x] (Math/abs (double x)))
+(defn pow [x y] (Math/pow (double x) (double y)))
+(defn exp [x] (Math/exp (double x)))
+
+(defn log
+  ([x] (Math/log (double x)))
+  ([x base] (/ (Math/log (double x)) (Math/log (double base)))))
+
+(defn log2 [x] (/ (Math/log (double x)) (Math/log 2.0)))
+(defn log10 [x] (Math/log10 (double x)))
+(defn log1p [x] (Math/log1p (double x)))
+(defn expm1 [x] (Math/expm1 (double x)))
+
+(defn sin [x]  (Math/sin (double x)))
+(defn cos [x]  (Math/cos (double x)))
+(defn tan [x]  (Math/tan (double x)))
+(defn asin [x] (Math/asin (double x)))
+(defn acos [x] (Math/acos (double x)))
+(defn atan [x] (Math/atan (double x)))
+(defn atan2 [y x] (Math/atan2 (double y) (double x)))
+
+(defn sinh [x]  (Math/sinh (double x)))
+(defn cosh [x]  (Math/cosh (double x)))
+(defn tanh [x]  (Math/tanh (double x)))
+(defn asinh [x] (Math/log (+ (double x) (Math/sqrt (+ (* (double x) (double x)) 1.0)))))
+(defn acosh [x] (Math/log (+ (double x) (Math/sqrt (- (* (double x) (double x)) 1.0)))))
+(defn atanh [x] (* 0.5 (Math/log (/ (+ 1.0 (double x)) (- 1.0 (double x))))))
+
+(defn degrees [x] (* (double x) (/ 180.0 Math/PI)))
+(defn radians [x] (* (double x) (/ Math/PI 180.0)))
+
+(defn hypot
+  ([x y] (Math/hypot (double x) (double y)))
+  ([x y & more] (Math/sqrt (reduce + (map #(* (double %) (double %)) (list* x y more))))))
+
+(defn isnan [x] (Double/isNaN (double x)))
+(defn isinf [x] (Double/isInfinite (double x)))
+(defn isfinite [x] (and (not (Double/isNaN (double x))) (not (Double/isInfinite (double x)))))
+(defn isclose
+  ([a b] (isclose a b 1e-9 0.0))
+  ([a b rel-tol] (isclose a b rel-tol 0.0))
+  ([a b rel-tol abs-tol]
+   (<= (Math/abs (- (double a) (double b)))
+       (max (* rel-tol (max (Math/abs (double a)) (Math/abs (double b)))) abs-tol))))
+
+(defn factorial [n]
+  (if (<= n 1) 1 (* (long n) (factorial (dec n)))))
+
+(defn gcd
+  ([a b]
+   (if (zero? b)
+     (Math/abs (long a))
+     (recur b (mod (long a) (long b)))))
+  ([a b & more]
+   (reduce gcd (gcd a b) more)))
+
+(defn lcm
+  ([a b]
+   (let [g (gcd a b)]
+     (if (zero? g) 0 (/ (* (Math/abs (long a)) (Math/abs (long b))) g))))
+  ([a b & more]
+   (reduce lcm (lcm a b) more)))
+
+(defn comb [n k]
+  (if (or (< k 0) (> k n))
+    0
+    (let [k (min k (- n k))]
+      (loop [i 0 result 1]
+        (if (= i k)
+          result
+          (recur (inc i) (/ (* result (- n i)) (inc i))))))))
+
+(defn perm
+  ([n] (factorial n))
+  ([n k]
+   (if (< k 0)
+     (throw (ex-info "k must be non-negative" {}))
+     (reduce * (range (- n k -1) (inc n))))))
+
+(defn prod [iterable]
+  (reduce * 1 iterable))
+
+(defn fsum [iterable]
+  (double (reduce + iterable)))
+
+(defn copysign [x y]
+  (Math/copySign (double x) (double y)))
+
+(defn frexp [x]
+  (if (zero? x)
+    [0.0 0]
+    (let [bits (Double/doubleToLongBits (double x))
+          exp  (bit-and (unsigned-bit-shift-right bits 52) 0x7FF)
+          mantissa (bit-or (bit-and bits (Long/parseUnsignedLong "000FFFFFFFFFFFFF" 16)) (Long/parseUnsignedLong "0010000000000000" 16))
+          m    (* (if (neg? x) -1.0 1.0) (/ mantissa (Math/pow 2.0 52)))
+          e    (- exp 1022)]
+      [(/ m 2.0) e])))
+
+(defn ldexp [x i]
+  (* (double x) (Math/pow 2.0 (double i))))
+
+(defn modf [x]
+  (let [d (double x)
+        i (Math/rint d)
+        f (- d i)]
+    [f (long i)]))
+
+(defn remainder [x y]
+  (let [d (double x)
+        v (double y)]
+    (- d (* (Math/rint (/ d v)) v))))
+
+(defn floor-div [x y]
+  (Math/floorDiv (long x) (long y)))
+
+(defn nextafter [x y]
+  (Math/nextAfter (double x) (double y)))
---
+++ test/conao3/battery/math_test.clj
@@ -0,0 +1,107 @@
+;; Original: Lib/test/test_math.py
+
+(ns conao3.battery.math-test
+  (:require
+   [clojure.test :as t]
+   [conao3.battery.math :as math-m]))
+
+(defn- approx= [a b]
+  (< (Math/abs (- (double a) (double b))) 1e-10))
+
+(t/deftest test-constants
+  (t/is (approx= 3.141592653589793 math-m/pi))
+  (t/is (approx= 2.718281828459045 math-m/e))
+  (t/is (approx= 6.283185307179586 math-m/tau))
+  (t/is (Double/isInfinite math-m/inf))
+  (t/is (Double/isNaN math-m/nan)))
+
+(t/deftest test-sqrt
+  (t/is (approx= 1.4142135623730951 (math-m/sqrt 2)))
+  (t/is (approx= 2.0 (math-m/sqrt 4)))
+  (t/is (approx= 3.0 (math-m/sqrt 9))))
+
+(t/deftest test-floor-ceil-trunc
+  (t/is (= 3 (math-m/floor 3.7)))
+  (t/is (= -4 (math-m/floor -3.2)))
+  (t/is (= 4 (math-m/ceil 3.2)))
+  (t/is (= -3 (math-m/ceil -3.7)))
+  (t/is (= 3 (math-m/trunc 3.7)))
+  (t/is (= -3 (math-m/trunc -3.7))))
+
+(t/deftest test-fabs
+  (t/is (= 3.7 (math-m/fabs -3.7)))
+  (t/is (= 3.7 (math-m/fabs 3.7))))
+
+(t/deftest test-pow-exp-log
+  (t/is (approx= 1024.0 (math-m/pow 2 10)))
+  (t/is (approx= math-m/e (math-m/exp 1)))
+  (t/is (approx= 1.0 (math-m/log math-m/e)))
+  (t/is (approx= 3.0 (math-m/log2 8)))
+  (t/is (approx= 2.0 (math-m/log10 100))))
+
+(t/deftest test-trig
+  (t/is (approx= 1.0 (math-m/sin (/ math-m/pi 2))))
+  (t/is (approx= 1.0 (math-m/cos 0)))
+  (t/is (approx= 1.0 (math-m/tan (/ math-m/pi 4))))
+  (t/is (approx= (/ math-m/pi 2) (math-m/asin 1)))
+  (t/is (approx= 0.0 (math-m/acos 1)))
+  (t/is (approx= (/ math-m/pi 4) (math-m/atan 1))))
+
+(t/deftest test-atan2
+  (t/is (approx= (/ math-m/pi 2) (math-m/atan2 1 0)))
+  (t/is (approx= math-m/pi (math-m/atan2 0 -1))))
+
+(t/deftest test-hypot
+  (t/is (approx= 5.0 (math-m/hypot 3 4)))
+  (t/is (approx= 5.0 (math-m/hypot -3 -4))))
+
+(t/deftest test-degrees-radians
+  (t/is (approx= 180.0 (math-m/degrees math-m/pi)))
+  (t/is (approx= math-m/pi (math-m/radians 180))))
+
+(t/deftest test-isnan-isinf-isfinite
+  (t/is (true? (math-m/isnan Double/NaN)))
+  (t/is (false? (math-m/isnan 1.0)))
+  (t/is (true? (math-m/isinf Double/POSITIVE_INFINITY)))
+  (t/is (false? (math-m/isinf 1.0)))
+  (t/is (true? (math-m/isfinite 1.0)))
+  (t/is (false? (math-m/isfinite Double/NaN))))
+
+(t/deftest test-factorial
+  (t/is (= 1 (math-m/factorial 0)))
+  (t/is (= 1 (math-m/factorial 1)))
+  (t/is (= 120 (math-m/factorial 5)))
+  (t/is (= 3628800 (math-m/factorial 10))))
+
+(t/deftest test-gcd
+  (t/is (= 4 (math-m/gcd 12 8)))
+  (t/is (= 1 (math-m/gcd 7 5)))
+  (t/is (= 6 (math-m/gcd 12 18))))
+
+(t/deftest test-lcm
+  (t/is (= 24 (math-m/lcm 8 12)))
+  (t/is (= 35 (math-m/lcm 5 7))))
+
+(t/deftest test-comb
+  (t/is (= 10 (math-m/comb 5 2)))
+  (t/is (= 1 (math-m/comb 5 0)))
+  (t/is (= 1 (math-m/comb 5 5)))
+  (t/is (= 0 (math-m/comb 5 6))))
+
+(t/deftest test-perm
+  (t/is (= 20 (math-m/perm 5 2)))
+  (t/is (= 120 (math-m/perm 5)))
+  (t/is (= 1 (math-m/perm 5 0))))
+
+(t/deftest test-isclose
+  (t/is (true? (math-m/isclose 1.0 1.0)))
+  (t/is (true? (math-m/isclose 1.0 1.0000000001)))
+  (t/is (false? (math-m/isclose 1.0 2.0))))
+
+(t/deftest test-copysign
+  (t/is (= -3.0 (math-m/copysign 3.0 -1.0)))
+  (t/is (= 3.0 (math-m/copysign -3.0 1.0))))
+
+(t/deftest test-log-with-base
+  (t/is (approx= 3.0 (math-m/log 8 2)))
+  (t/is (approx= 2.0 (math-m/log 100 10))))
