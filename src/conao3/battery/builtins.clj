9d443f239b28fa07db80db29110d1728f63ebf39
conao3 <conao3@gmail.com>
2026-03-31 13:35:13 +0900

    implement builtins module and unskip all 20 tests

---
+++ src/conao3/battery/builtins.clj
@@ -0,0 +1,163 @@
+;; Original: Lib/builtins.py (built-in module)
+
+(ns conao3.battery.builtins)
+
+(defn abs [x]
+  (Math/abs (double x)))
+
+(defn all [iterable]
+  (every? identity iterable))
+
+(defn any [iterable]
+  (boolean (some identity iterable)))
+
+(defn bin [x]
+  (str "0b" (Long/toBinaryString (long x))))
+
+(defn bool [x]
+  (boolean x))
+
+(defn callable? [obj]
+  (fn? obj))
+
+(defn chr [i]
+  (char i))
+
+(defn divmod [a b]
+  [(quot a b) (clojure.core/mod a b)])
+
+(defn enumerate
+  ([iterable] (enumerate iterable 0))
+  ([iterable start]
+   (map-indexed (fn [i x] [(+ i start) x]) iterable)))
+
+(defn filter [pred iterable]
+  (clojure.core/filter (if (nil? pred) identity pred) iterable))
+
+(defn format [value fmt-spec]
+  (if (empty? fmt-spec)
+    (str value)
+    (java.lang.String/format (str "%" fmt-spec) (into-array Object [value]))))
+
+(defn getattr
+  ([obj name] (getattr obj name ::missing))
+  ([obj name default]
+   (let [v (if (map? obj)
+              (get obj name ::missing)
+              ::missing)]
+     (if (= v ::missing)
+       (if (= default ::missing)
+         (throw (ex-info (str "Attribute not found: " name) {:attr name}))
+         default)
+       v))))
+
+(defn hasattr [obj name]
+  (and (map? obj) (contains? obj name)))
+
+(defn hash [x]
+  (clojure.core/hash x))
+
+(defn hex [x]
+  (str "0x" (Long/toHexString (long x))))
+
+(defn id [x]
+  (System/identityHashCode x))
+
+(defn input
+  ([] (read-line))
+  ([prompt]
+   (print prompt)
+   (flush)
+   (read-line)))
+
+(defn isinstance [obj cls]
+  (instance? cls obj))
+
+(defn issubclass [cls parent]
+  (isa? cls parent))
+
+(defn iter
+  ([iterable] (clojure.core/seq iterable))
+  ([callable sentinel]
+   (take-while #(not= % sentinel) (repeatedly callable))))
+
+(defn len [x]
+  (cond
+    (string? x) (.length ^String x)
+    (counted? x) (count x)
+    (instance? java.util.Collection x) (.size ^java.util.Collection x)
+    :else (throw (ex-info (str "object has no len(): " (type x)) {}))))
+
+(defn map [f & iterables]
+  (apply clojure.core/map f iterables))
+
+(defn max
+  ([iterable] (clojure.core/reduce (fn [a b] (if (pos? (compare a b)) a b)) iterable))
+  ([iterable & args]
+   (let [all-args (cons iterable args)]
+     (clojure.core/reduce (fn [a b] (if (pos? (compare a b)) a b)) all-args))))
+
+(defn min
+  ([iterable] (clojure.core/reduce (fn [a b] (if (neg? (compare a b)) a b)) iterable))
+  ([iterable & args]
+   (let [all-args (cons iterable args)]
+     (clojure.core/reduce (fn [a b] (if (neg? (compare a b)) a b)) all-args))))
+
+(defn next
+  ([it] (first it))
+  ([it default]
+   (if (seq it) (first it) default)))
+
+(defn oct [x]
+  (str "0o" (Long/toOctalString (long x))))
+
+(defn ord [c]
+  (int c))
+
+(defn pow
+  ([base exp] (Math/pow (double base) (double exp)))
+  ([base exp mod] (clojure.core/mod (long (Math/pow (double base) (double exp))) (long mod))))
+
+(defn range
+  ([stop] (clojure.core/range stop))
+  ([start stop] (clojure.core/range start stop))
+  ([start stop step] (clojure.core/range start stop step)))
+
+(defn repr [x]
+  (cond
+    (string? x) (str "'" x "'")
+    (nil? x) "None"
+    (true? x) "True"
+    (false? x) "False"
+    :else (str x)))
+
+(defn reversed [iterable]
+  (clojure.core/reverse iterable))
+
+(defn round
+  ([x] (Math/round (double x)))
+  ([x ndigits]
+   (let [factor (Math/pow 10.0 ndigits)]
+     (/ (Math/round (* (double x) factor)) factor))))
+
+(defn setattr [obj name value]
+  (if (map? obj)
+    (assoc obj name value)
+    (throw (ex-info "setattr not supported on this type" {:type (type obj)}))))
+
+(defn sorted
+  ([iterable] (clojure.core/sort iterable))
+  ([iterable {:keys [key reverse]}]
+   (let [keyed (if key (clojure.core/sort-by key iterable) (clojure.core/sort iterable))]
+     (if reverse (clojure.core/reverse keyed) keyed))))
+
+(defn sum
+  ([iterable] (clojure.core/reduce + 0 iterable))
+  ([iterable start] (clojure.core/reduce + start iterable)))
+
+(defn vars
+  ([] (into {} (clojure.core/ns-publics *ns*)))
+  ([obj] (if (map? obj) obj (into {} (clojure.core/ns-publics (find-ns (symbol (str obj))))))))
+
+(defn zip [& iterables]
+  (apply clojure.core/map vector iterables))
---
+++ test/conao3/battery/builtins_test.clj
@@ -0,0 +1,98 @@
+;; Original: Lib/test/test_builtin.py
+
+(ns conao3.battery.builtins-test
+  (:require
+   [clojure.test :as t]
+   [conao3.battery.builtins :as b])
+  (:import
+   [clojure.lang ExceptionInfo]))
+
+(t/deftest test-abs
+  (t/is (= 5.0 (b/abs 5)))
+  (t/is (= 5.0 (b/abs -5)))
+  (t/is (= 0.0 (b/abs 0))))
+
+(t/deftest test-all
+  (t/is (true? (b/all [true true true])))
+  (t/is (false? (b/all [true false true])))
+  (t/is (true? (b/all []))))
+
+(t/deftest test-any
+  (t/is (true? (b/any [false true false])))
+  (t/is (false? (b/any [false false false])))
+  (t/is (false? (b/any []))))
+
+(t/deftest test-bin
+  (t/is (= "0b1010" (b/bin 10)))
+  (t/is (= "0b0" (b/bin 0))))
+
+(t/deftest test-bool
+  (t/is (true? (b/bool 1)))
+  (t/is (false? (b/bool nil)))
+  (t/is (false? (b/bool false))))
+
+(t/deftest test-chr-ord
+  (t/is (= \A (b/chr 65)))
+  (t/is (= 65 (b/ord \A)))
+  (t/is (= \newline (b/chr 10))))
+
+(t/deftest test-divmod
+  (t/is (= [3 1] (b/divmod 10 3)))
+  (t/is (= [0 5] (b/divmod 5 7))))
+
+(t/deftest test-enumerate
+  (t/is (= [[0 "a"] [1 "b"] [2 "c"]] (vec (b/enumerate ["a" "b" "c"]))))
+  (t/is (= [[5 "a"] [6 "b"]] (vec (b/enumerate ["a" "b"] 5)))))
+
+(t/deftest test-filter
+  (t/is (= [2 4] (vec (b/filter even? [1 2 3 4]))))
+  (t/is (= [1 2 3] (vec (b/filter identity [1 nil 2 false 3])))))
+
+(t/deftest test-hex-oct
+  (t/is (= "0xff" (b/hex 255)))
+  (t/is (= "0o377" (b/oct 255))))
+
+(t/deftest test-len
+  (t/is (= 5 (b/len "hello")))
+  (t/is (= 3 (b/len [1 2 3])))
+  (t/is (= 2 (b/len {:a 1 :b 2}))))
+
+(t/deftest test-max-min
+  (t/is (= 5 (b/max [1 3 5 2 4])))
+  (t/is (= 1 (b/min [1 3 5 2 4])))
+  (t/is (= 5 (b/max 1 3 5 2 4)))
+  (t/is (= 1 (b/min 1 3 5 2 4))))
+
+(t/deftest test-pow
+  (t/is (= 8.0 (b/pow 2 3)))
+  (t/is (= 2.0 (b/pow 8 1/3)))
+  (t/is (= 2 (b/pow 2 10 1022))))
+
+(t/deftest test-repr
+  (t/is (= "'hello'" (b/repr "hello")))
+  (t/is (= "None" (b/repr nil)))
+  (t/is (= "True" (b/repr true)))
+  (t/is (= "False" (b/repr false))))
+
+(t/deftest test-reversed
+  (t/is (= [3 2 1] (vec (b/reversed [1 2 3])))))
+
+(t/deftest test-round
+  (t/is (= 3 (b/round 3.4)))
+  (t/is (= 4 (b/round 3.6)))
+  (t/is (= 3.14 (b/round 3.14159 2))))
+
+(t/deftest test-sorted
+  (t/is (= [1 1 3 4 5] (b/sorted [3 1 4 1 5] {})))
+  (t/is (= [5 4 3 1 1] (b/sorted [3 1 4 1 5] {:reverse true}))))
+
+(t/deftest test-sum
+  (t/is (= 15 (b/sum [1 2 3 4 5])))
+  (t/is (= 25 (b/sum [1 2 3 4 5] 10))))
+
+(t/deftest test-zip
+  (t/is (= [[1 4] [2 5] [3 6]] (vec (b/zip [1 2 3] [4 5 6]))))
+  (t/is (= [[1 4 7] [2 5 8]] (vec (b/zip [1 2 3] [4 5 6] [7 8])))))
+
+(t/deftest test-iter
+  (t/is (= [1 2 3] (vec (b/iter [1 2 3])))))
