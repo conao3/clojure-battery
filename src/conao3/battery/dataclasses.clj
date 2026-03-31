513071cc7bb811f63043aab4709ff0d051aa5135
conao3 <conao3@gmail.com>
2026-03-31 13:48:00 +0900

    implement dataclasses module and add 8 tests

--- src/conao3/battery/dataclasses.clj
+++ src/conao3/battery/dataclasses.clj
@@ -1,1 +1,43 @@
-(ns conao3.battery.dataclasses)
+(ns conao3.battery.dataclasses
+  (:require [clojure.string :as str]))
+
+(def dataclass-registry (atom #{}))
+
+(defn register-dataclass! [cls]
+  (swap! dataclass-registry conj cls))
+
+(defmacro defdata
+  [name field-syms]
+  `(do
+     (defrecord ~name ~field-syms)
+     (conao3.battery.dataclasses/register-dataclass! ~name)
+     '~name))
+
+(defn is-dataclass?
+  [obj]
+  (boolean
+   (when obj
+     (contains? @dataclass-registry (if (class? obj) obj (class obj))))))
+
+(defn fields
+  [obj]
+  (let [inst (if (class? obj) nil obj)]
+    (when inst
+      (vec (keys inst)))))
+
+(defn asdict
+  [obj]
+  (into {} obj))
+
+(defn astuple
+  [obj]
+  (vec (vals obj)))
+
+(defn replace
+  [obj changes]
+  (merge obj changes))
+
+(defn field
+  [& {:keys [default default-factory]}]
+  {:default default
+   :default-factory default-factory})
--- test/conao3/battery/dataclasses_test.clj
+++ test/conao3/battery/dataclasses_test.clj
@@ -5,3 +5,43 @@
   (:require
    [clojure.test :as t]
    [conao3.battery.dataclasses :as dataclasses]))
+
+(dataclasses/defdata Point [x y])
+(dataclasses/defdata Person [name age])
+
+(t/deftest test-defdata-creation
+  (let [p (->Point 3 4)]
+    (t/is (= 3 (:x p)))
+    (t/is (= 4 (:y p)))))
+
+(t/deftest test-defdata-equality
+  (t/is (= (->Point 1 2) (->Point 1 2)))
+  (t/is (not= (->Point 1 2) (->Point 1 3))))
+
+(t/deftest test-is-dataclass
+  (t/is (true? (dataclasses/is-dataclass? (->Point 1 2))))
+  (t/is (true? (dataclasses/is-dataclass? (->Person "Alice" 30))))
+  (t/is (false? (dataclasses/is-dataclass? {:x 1 :y 2})))
+  (t/is (false? (dataclasses/is-dataclass? "string"))))
+
+(t/deftest test-fields
+  (t/is (= [:x :y] (dataclasses/fields (->Point 1 2))))
+  (t/is (= [:name :age] (dataclasses/fields (->Person "Alice" 30)))))
+
+(t/deftest test-asdict
+  (t/is (= {:x 1 :y 2} (dataclasses/asdict (->Point 1 2))))
+  (t/is (= {:name "Alice" :age 30} (dataclasses/asdict (->Person "Alice" 30)))))
+
+(t/deftest test-astuple
+  (t/is (= [1 2] (dataclasses/astuple (->Point 1 2))))
+  (t/is (= ["Bob" 25] (dataclasses/astuple (->Person "Bob" 25)))))
+
+(t/deftest test-replace
+  (let [p (->Point 1 2)
+        p2 (dataclasses/replace p {:x 10})]
+    (t/is (= 10 (:x p2)))
+    (t/is (= 2 (:y p2)))))
+
+(t/deftest test-field-default
+  (let [f (dataclasses/field :default 42)]
+    (t/is (= 42 (:default f)))))
