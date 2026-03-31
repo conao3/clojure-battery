00c86b25d1b0621668928db3c0021c67adb75442
conao3 <conao3@gmail.com>
2026-03-31 13:49:06 +0900

    implement enum module and add 8 tests

--- src/conao3/battery/enum.clj
+++ src/conao3/battery/enum.clj
@@ -1,1 +1,57 @@
 (ns conao3.battery.enum)
+
+(def enum-registry (atom #{}))
+
+(defrecord EnumMember [enum-name member-name value])
+
+(defmethod print-method EnumMember [em w]
+  (.write w (str "<" (:enum-name em) "." (:member-name em) ": " (:value em) ">")))
+
+(defn register-enum! [cls]
+  (swap! enum-registry conj cls))
+
+(def ^:private auto-counter (atom 0))
+
+(defn auto []
+  (swap! auto-counter inc)
+  @auto-counter)
+
+(defmacro defenum
+  [enum-name & pairs]
+  (let [entries (partition 2 pairs)
+        members (mapv (fn [[k v]]
+                        [(keyword k)
+                         `(->EnumMember ~(str enum-name) ~(str k) ~v)])
+                      entries)]
+    `(do
+       (def ~enum-name
+         ~(into {} members))
+       (conao3.battery.enum/register-enum! ~enum-name)
+       '~enum-name)))
+
+(defn is-enum?
+  [obj]
+  (boolean (contains? @enum-registry obj)))
+
+(defn enum-name
+  [member]
+  (:member-name member))
+
+(defn value
+  [member]
+  (:value member))
+
+(defn from-value
+  [enum val]
+  (some (fn [[_ m]] (when (= (:value m) val) m)) enum))
+
+(defn members
+  [enum]
+  (vec (vals enum)))
+
+(defn unique
+  [enum]
+  (let [vals (mapv :value (vals enum))]
+    (when (not= (count vals) (count (set vals)))
+      (throw (ex-info "Duplicate values found in enum" {:enum enum})))
+    enum))
--- test/conao3/battery/enum_test.clj
+++ test/conao3/battery/enum_test.clj
@@ -4,4 +4,53 @@
 (ns conao3.battery.enum-test
   (:require
    [clojure.test :as t]
-   [conao3.battery.enum :as enum]))
+   [conao3.battery.enum :as enum])
+  (:import
+   [clojure.lang ExceptionInfo]))
+
+(enum/defenum Color
+  RED   1
+  GREEN 2
+  BLUE  3)
+
+(enum/defenum Direction
+  NORTH "north"
+  SOUTH "south"
+  EAST  "east"
+  WEST  "west")
+
+(t/deftest test-defenum-creation
+  (t/is (map? Color))
+  (t/is (contains? Color :RED))
+  (t/is (contains? Color :GREEN))
+  (t/is (contains? Color :BLUE)))
+
+(t/deftest test-enum-value
+  (t/is (= 1 (enum/value (:RED Color))))
+  (t/is (= 2 (enum/value (:GREEN Color))))
+  (t/is (= 3 (enum/value (:BLUE Color)))))
+
+(t/deftest test-enum-name
+  (t/is (= "RED" (enum/enum-name (:RED Color))))
+  (t/is (= "GREEN" (enum/enum-name (:GREEN Color)))))
+
+(t/deftest test-string-enum
+  (t/is (= "north" (enum/value (:NORTH Direction))))
+  (t/is (= "NORTH" (enum/enum-name (:NORTH Direction)))))
+
+(t/deftest test-from-value
+  (t/is (= (:RED Color) (enum/from-value Color 1)))
+  (t/is (= (:BLUE Color) (enum/from-value Color 3)))
+  (t/is (nil? (enum/from-value Color 99))))
+
+(t/deftest test-members
+  (t/is (= 3 (count (enum/members Color))))
+  (t/is (= 4 (count (enum/members Direction)))))
+
+(t/deftest test-unique-valid
+  (t/is (map? (enum/unique Color))))
+
+(t/deftest test-unique-duplicate
+  (let [dup-enum {:A (enum/->EnumMember "Dup" "A" 1)
+                  :B (enum/->EnumMember "Dup" "B" 1)}]
+    (t/is (thrown? ExceptionInfo (enum/unique dup-enum)))))
