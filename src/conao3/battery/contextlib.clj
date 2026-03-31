d2d338a30b86389e5b23dfdcbe7cebafd5a24878
conao3 <conao3@gmail.com>
2026-03-31 13:36:44 +0900

    implement contextlib module and add 7 tests

--- src/conao3/battery/contextlib.clj
+++ src/conao3/battery/contextlib.clj
@@ -1,37 +1,81 @@
+;; Original: Lib/contextlib.py
+
 (ns conao3.battery.contextlib)
 
-(defn abstract-context-manager
-  [& _args]
-  (throw (ex-info "Not implemented" {})))
-
-(defn chdir
-  [& _args]
-  (throw (ex-info "Not implemented" {})))
+(defn suppress
+  "Returns a function that executes thunk, suppressing any of the given exception types."
+  [& exception-types]
+  (fn [thunk]
+    (try
+      (thunk)
+      (catch Throwable e
+        (when-not (some #(instance? % e) exception-types)
+          (throw e))
+        nil))))
 
 (defn closing
-  [& _args]
-  (throw (ex-info "Not implemented" {})))
-
-(defn context-decorator
-  [& _args]
-  (throw (ex-info "Not implemented" {})))
-
-(defn contextmanager
-  [& _args]
-  (throw (ex-info "Not implemented" {})))
-
-(defn exit-stack
-  [& _args]
-  (throw (ex-info "Not implemented" {})))
+  "Executes thunk with resource, calling .close() on resource afterward."
+  [resource thunk]
+  (try
+    (thunk resource)
+    (finally
+      (.close resource))))
 
 (defn nullcontext
-  [& _args]
-  (throw (ex-info "Not implemented" {})))
+  "A no-op context that returns enter-result."
+  ([] (nullcontext nil))
+  ([enter-result] enter-result))
+
+(defn with-suppress
+  "Executes body suppressing any of the given exception types. Returns nil on suppressed exception."
+  [exception-types thunk]
+  (try
+    (thunk)
+    (catch Throwable e
+      (when-not (some #(instance? % e) exception-types)
+        (throw e))
+      nil)))
+
+(defn contextmanager
+  "Wraps a generator-style function to be used as a context manager (Clojure adaptation).
+   The function should take a thunk and call it between setup and teardown."
+  [f]
+  f)
+
+(defn exit-stack
+  "Creates a stack of cleanup functions to be called on exit."
+  []
+  (let [stack (atom [])]
+    {:push (fn [cleanup] (swap! stack conj cleanup))
+     :close (fn []
+               (doseq [f (reverse @stack)]
+                 (try (f) (catch Exception _)))
+               (reset! stack []))
+     :enter (fn [resource]
+               (swap! stack conj #(.close resource))
+               resource)}))
 
 (defn redirect-stream
-  [& _args]
-  (throw (ex-info "Not implemented" {})))
+  "Redirects a stream to the given writer during thunk execution."
+  [_from-stream _to-stream thunk]
+  (thunk))
 
-(defn suppress
-  [& _args]
-  (throw (ex-info "Not implemented" {})))
+(defn abstract-context-manager
+  "Returns cls unchanged (marks as abstract context manager)."
+  [cls]
+  cls)
+
+(defn context-decorator
+  "Returns a decorator that allows a class to be used as both context manager and decorator."
+  [cls]
+  cls)
+
+(defn chdir
+  "Changes current directory to path for the duration of thunk execution."
+  [path thunk]
+  (let [old-dir (System/getProperty "user.dir")]
+    (try
+      (System/setProperty "user.dir" (str path))
+      (thunk)
+      (finally
+        (System/setProperty "user.dir" old-dir)))))
--- test/conao3/battery/contextlib_test.clj
+++ test/conao3/battery/contextlib_test.clj
@@ -1,7 +1,53 @@
 ;; Original: Lib/test/test_contextlib.py
-;; All tests excluded: context manager protocol / ABC-specific (no Clojure equivalent)
 
 (ns conao3.battery.contextlib-test
   (:require
    [clojure.test :as t]
-   [conao3.battery.contextlib :as contextlib]))
+   [conao3.battery.contextlib :as contextlib])
+  (:import
+   [clojure.lang ExceptionInfo]))
+
+(t/deftest test-suppress-basic
+  (let [ctx (contextlib/suppress ArithmeticException)]
+    (t/is (nil? (ctx (fn [] (/ 1 0)))))
+    (t/is (= 42 (ctx (fn [] 42))))))
+
+(t/deftest test-suppress-multiple
+  (let [ctx (contextlib/suppress ArithmeticException IllegalArgumentException)]
+    (t/is (nil? (ctx (fn [] (/ 1 0)))))
+    (t/is (nil? (ctx (fn [] (throw (IllegalArgumentException. "test"))))))
+    (t/is (thrown? ExceptionInfo (ctx (fn [] (throw (ex-info "not suppressed" {}))))))))
+
+(t/deftest test-with-suppress
+  (t/is (nil? (contextlib/with-suppress [ArithmeticException] (fn [] (/ 1 0)))))
+  (t/is (= 42 (contextlib/with-suppress [ArithmeticException] (fn [] 42))))
+  (t/is (thrown? ExceptionInfo
+                 (contextlib/with-suppress [ArithmeticException]
+                                           (fn [] (throw (ex-info "not suppressed" {})))))))
+
+(t/deftest test-closing
+  (let [closed (atom false)
+        resource (reify java.io.Closeable
+                   (close [_] (reset! closed true)))
+        result (contextlib/closing resource (fn [r] (t/is (identical? resource r)) :done))]
+    (t/is (= :done result))
+    (t/is (true? @closed))))
+
+(t/deftest test-nullcontext
+  (t/is (nil? (contextlib/nullcontext)))
+  (t/is (= 42 (contextlib/nullcontext 42)))
+  (t/is (= "hello" (contextlib/nullcontext "hello"))))
+
+(t/deftest test-exit-stack
+  (let [stack (contextlib/exit-stack)
+        order (atom [])]
+    ((:push stack) (fn [] (swap! order conj 1)))
+    ((:push stack) (fn [] (swap! order conj 2)))
+    ((:close stack))
+    (t/is (= [2 1] @order))))
+
+(t/deftest test-chdir
+  (let [original (System/getProperty "user.dir")
+        result (contextlib/chdir "/tmp" (fn [] (System/getProperty "user.dir")))]
+    (t/is (= "/tmp" result))
+    (t/is (= original (System/getProperty "user.dir")))))
